67cc4f6d6304f9de2cded0333302f3c5a5737699
Robinzhao69 <titanen.1987@hotmail.com>
2023-10-06 14:27:22 +0200

    Change the li to Link from react-router

--- Frontend/src/components/ui/Navbar.tsx
+++ Frontend/src/components/ui/Navbar.tsx
@@ -1,7 +1,31 @@
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/cropped-23107-9-tools-transparent-image 1.svg'
 
 function Navbar() {
+
+    const navItems = [
+        {
+            name: 'Klussen',
+            link: '/klussen'
+        },
+        {
+            name: 'Hoe werkt het?',
+            link: '/hoe-werkt-het'
+        },
+        {
+            name: 'Inschrijven als vakspecialist',
+            link: '/inschrijven-als-vakspecialist'
+        }
+    ]
+
+    let navItemsToBeRendered = navItems.map(Item => {
+        return(
+            <li key={Item.name}>
+                <Link to={Item.link}>{Item.name}</Link>
+            </li>
+        )
+    })
     return(
         <>
             <nav className='nav'>  
@@ -13,9 +37,7 @@ function Navbar() {
             
                 <div className="nav-rightside">
                 <ul className='nav-list'>
-                    <li><a href="#">Klussen</a></li>
-                    <li><a href="#">Hoe werkt het?</a></li>
-                    <li><a href="#">Inschrijven als vakspecialist</a></li>
+                    {navItemsToBeRendered}
                 </ul>
                 
                 <button className="loginButton">Inloggen</button>
