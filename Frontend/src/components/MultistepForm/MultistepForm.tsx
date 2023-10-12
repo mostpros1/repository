9ee296ef15a8757b97a8b1f7bc39aeb02ee9c71f
Robinzhao69 <titanen.1987@hotmail.com>
2023-10-12 15:12:23 +0200

    Added progress bar to the multistepform

--- Frontend/src/components/MultistepForm/MultistepForm.tsx
+++ Frontend/src/components/MultistepForm/MultistepForm.tsx
@@ -12,9 +12,22 @@ function MultistepForm() {
   const { step, steps, currentStepIndex, isFirstStep, isLastStep, back, next} = useMultistepForm([<AddressForm />, <CategoryForm />, <InfoForm />, <EmailForm />
   ])
 
+  const stepWidth = 100 / steps.length;
+
   return (
     <div className='form-con'>
-        <div>Stap {currentStepIndex + 1} van {steps.length}</div>
+        <div className='progress-con'>
+          <h3>Stap {currentStepIndex + 1} van {steps.length}</h3>
+          <div className="progress-bar">
+            {steps.map((_, index) => (
+              <div
+                key={index}
+                className={`progress-step ${index <= currentStepIndex ? "active" : ""}`}
+                style={{ width: `${stepWidth}%` }}
+              ></div>
+            ))}
+          </div>
+        </div>     
         {step}
         <div className='btn-wrapper'>
           {!isFirstStep && <button onClick={back} className='form-btn back'>Vorige</button>}  
