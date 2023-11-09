4cf09d2a3fbfdd3e89d846cce47b5d7131b0c75d
Jelani Alexis <jelanialexis5@gmail.com>
2023-11-09 17:22:57 +0100

    add amplify backend, rename choresdescription

--- Frontend/src/pages/HomePage/HomePage.tsx
+++ Frontend/src/pages/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@ import NavBar from "../../components/ui/NavBar/NavBar"
 import PopularCardsSection from "../../components/PopularCards/PopularCardsSection"
 import Hero from "../../components/Hero/Hero"
 import MostprosDesc from "../../components/MostprosDesc/MostprosDesc"
-import ChoresDesc from "../../components/ChoresDesc/ChoresDesc"
+import ChoresDescription from "../../components/ChoresDescription/ChoresDescription"
 import Characteristics from "../../components/Characteristics/Characteristics"
 import Reviews from "../../components/Reviews/Reviews"
 import Footer from "../../components/ui/Footer/Footer"
@@ -20,7 +20,7 @@ function HomePage() {
         {/* Popular Jobs */}
         <PopularCardsSection />
         {/* Find specialist for every job */}
-        <ChoresDesc />
+        <ChoresDescription />
         {/* Why Mostpros */}
         <Characteristics/>
         {/* Reviews */}
