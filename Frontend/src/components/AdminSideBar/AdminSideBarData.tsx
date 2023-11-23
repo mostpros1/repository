125f9dc11c991b7edd02421148b3e7cfeca37284
Timo Schuurman <timoschuurman@outlook.com>
2023-11-23 14:50:46 +0100

    Admin Paneel 0.3
    een file bij toegevoegd, met niks erin.

--- Frontend/src/components/AdminSideBar/AdminSideBarData.tsx
+++ Frontend/src/components/AdminSideBar/AdminSideBarData.tsx
@@ -1,6 +1,5 @@
-import * as FaIcons from 'react-icons/fa'
+
 import * as AiIcons from 'react-icons/ai'
-import * as IoIcons from 'react-icons/io'
 import * as RiIcons from 'react-icons/ri'
 
 export const AdminSideBarData = [
