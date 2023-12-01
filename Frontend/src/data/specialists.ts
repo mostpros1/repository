07108a290e971031adcbd42596eb2841105f34f7
Robin Zhao <titanen.1987@hotmail.com>
2023-12-01 17:21:58 +0100

    add new filter function to the searchbar

---
+++ Frontend/src/data/specialists.ts
@@ -0,0 +1,52 @@
+const specialists = [
+    {
+        id: 1,
+        name: "Loodgieter",
+        tasks: [
+            { task: "Lekkages repareren", link: "/lekkages-repareren" },
+            { task: "Sanitair installeren", link: "/sanitair-installeren" },
+            // Voeg meer taken toe indien nodig
+        ]
+    },
+    {
+        id: 2,
+        name: "Hovenier",
+        tasks: [
+            { task: "Tuinontwerp maken", link: "/tuinontwerp-maken" },
+            { task: "Planten verzorgen", link: "/planten-verzorgen" },
+            // Voeg meer taken toe indien nodig
+        ]
+    },
+    {
+        id: 3,
+        name: "Elektriciën",
+        tasks: [
+            { task: "Elektrische bedrading repareren", link: "/elektrische-bedrading-repareren" },
+            { task: "Stopcontacten installeren", link: "/stopcontacten-installeren" },
+            // Voeg meer taken toe indien nodig
+        ]
+    },
+    {
+        id: 4,
+        name: "Dakdekker",
+        tasks: [
+            { task: "Dakreparaties uitvoeren", link: "/dakreparaties-uitvoeren" },
+            { task: "Dakbedekking vervangen", link: "/dakbedekking-vervangen" },
+            // Voeg meer taken toe indien nodig
+        ]
+    },
+    {
+        id: 5,
+        name: "Schoonmaker",
+        tasks: [
+            { task: "Schoonmaken van woningen", link: "/schoonmaken-van-woningen" },
+            { task: "Ramen wassen", link: "/ramen-wassen" },
+            // Voeg meer taken toe indien nodig
+        ]
+    },
+    // Voeg hier meer specialisten toe met hun specifieke taken
+];
+
+
+
+export default specialists
