007f4e9bb044d18e9d72cb11e7ae651e390d492e
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2023-12-06 14:12:09 +0100

    Save for merge

--- App/app/App.tsx
+++ App/app/App.tsx
@@ -17,65 +17,74 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { Amplify, Auth } from "aws-amplify";
 import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
-const Stack = createNativeStackNavigator();
+
 
 const App = () => {
-  type RootStackParamList = {
-    Screen1: undefined;
-    Screen2: { itemId: number; otherParam: string };
-  };
+//   type RootStackParamList = {
+//     LandingPage: undefined;
+//     KeuzePage: undefined;
+//     HowItWorksOneHomeowner: undefined;
+//     HowItWorksTwoHomeowner: undefined;
+//     HowItWorksThreeHomeowner: undefined;
+//     HowItWorksOneSpecialist: undefined;
+//     HowItWorksTwoSpecialist: undefined;
+//     HowItWorksThreeSpecialist: undefined;
+//     Home: undefined;
+//     Register: undefined;
+//     Login: undefined;
+//   };
+//   const Stack = createNativeStackNavigator<RootStackParamList>();
+//   type Screen1NavigationProp = StackNavigationProp<
+//     RootStackParamList,
+//     "Screen1"
+//   >;
+//   type Screen1RouteProp = RouteProp<RootStackParamList, "Screen1">;
 
-  type Screen1NavigationProp = StackNavigationProp<
-    RootStackParamList,
-    "Screen1"
-  >;
-  type Screen1RouteProp = RouteProp<RootStackParamList, "Screen1">;
-
-  type Screen2NavigationProp = StackNavigationProp<
-    RootStackParamList,
-    "Screen2"
-  >;
-  type Screen2RouteProp = RouteProp<RootStackParamList, "Screen2">;
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LandingPage"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="LandingPage" component={LandingPage} />
-        <Stack.Screen name="KeuzePage" component={KeuzePage} />
-        <Stack.Screen
-          name="howItWorksOneHomeowner"
-          component={howItWorksOneHomeowner}
-        />
-        <Stack.Screen
-          name="howItWorksTwoHomeowner"
-          component={howItWorksTwoHomeowner}
-        />
-        <Stack.Screen
-          name="howItWorksThreeHomeowner"
-          component={howItWorksThreeHomeowner}
-        />
-        <Stack.Screen
-          name="howItWorksOneSpecialist"
-          component={howItWorksOneSpecialist}
-        />
-        <Stack.Screen
-          name="howItWorksTwoSpecialist"
-          component={howItWorksTwoSpecialist}
-        />
-        <Stack.Screen
-          name="howItWorksThreeSpecialist"
-          component={howItWorksThreeSpecialist}
-        />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-        {/* <Stack.Screen name="Verify" component={Verify} /> */}
-        {/* <Stack.Screen name="" component={} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+//   type Screen2NavigationProp = StackNavigationProp<
+//     RootStackParamList,
+//     "Screen2"
+//   >;
+//   type Screen2RouteProp = RouteProp<RootStackParamList, "Screen2">;
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="LandingPage"
+//         screenOptions={{ headerShown: false }}
+//       >
+//         <Stack.Screen name="LandingPage" component={LandingPage} />
+//         <Stack.Screen name="KeuzePage" component={KeuzePage} />
+//         <Stack.Screen
+//           name="howItWorksOneHomeowner"
+//           component={howItWorksOneHomeowner}
+//         />
+//         <Stack.Screen
+//           name="howItWorksTwoHomeowner"
+//           component={howItWorksTwoHomeowner}
+//         />
+//         <Stack.Screen
+//           name="howItWorksThreeHomeowner"
+//           component={howItWorksThreeHomeowner}
+//         />
+//         <Stack.Screen
+//           name="howItWorksOneSpecialist"
+//           component={howItWorksOneSpecialist}
+//         />
+//         <Stack.Screen
+//           name="howItWorksTwoSpecialist"
+//           component={howItWorksTwoSpecialist}
+//         />
+//         <Stack.Screen
+//           name="howItWorksThreeSpecialist"
+//           component={howItWorksThreeSpecialist}
+//         />
+//         <Stack.Screen name="Home" component={Home} />
+//         <Stack.Screen name="Register" component={Register} />
+//         <Stack.Screen name="Login" component={Login} />
+//         {/* <Stack.Screen name="Verify" component={Verify} /> */}
+//         {/* <Stack.Screen name="" component={} /> */}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
 };
 
 export default App;
