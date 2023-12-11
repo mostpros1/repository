868bc72dcd8063508b89cdbbb4581ef02facd478
Jelani Alexis <jelanialexis5@gmail.com>
2023-12-11 13:38:50 +0100

    resolve post-build errors

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
   define: {
     _global: ({})
   }
