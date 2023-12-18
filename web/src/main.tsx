edca1b9851a42d46ea33cff0f889ab197e0fa8e6
Robin Zhao <titanen.1987@hotmail.com>
2023-12-18 15:17:21 +0100

    remove aws install

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
 import { Amplify } from 'aws-amplify';
-import awsExports from '../aws-exports.js';
+// import awsExports from '../aws-exports.js';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App'
 import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
