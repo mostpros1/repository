28ebabe959eb9548d36c9e91d5b8d20be55e5a00
Robin Zhao <titanen.1987@hotmail.com>
2024-01-02 16:40:02 +0100

    add basic structure of redux in loginpage

--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,9 @@
 import  { useState } from 'react';
+// LoginPage.js
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../../actions/userActions';
+import { RootState } from '../../store';
+
 import NavBar from '../../components/ui/NavBar/NavBar';
 import { LoginForm } from '../../components/MultistepForm/LoginForm';
 import Footer from '../../components/ui/Footer/Footer';
@@ -6,6 +11,9 @@ import Footer from '../../components/ui/Footer/Footer';
 import './LoginPage.css';
 
 function LoginPage() {
+
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user);
 
   const [loginData, setLoginData] = useState({
     email: '',
