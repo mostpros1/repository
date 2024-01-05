5276fc8eb5b71bffa153feadbaeca7e08de62f4c
Jelani Alexis <jelanialexis5@gmail.com>
2024-01-05 16:37:39 +0100

    add useAuthorization hook, ProtectedRoute now uses it

--- web/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ web/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,30 +1,30 @@
-import { Auth } from "aws-amplify";
 import { ReactNode, useEffect, useState } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
-
-type UserRoles = "Admin" | "Homeowner" | "Professional"
+import { useAuthorization } from "../../hooks/useAuthorization";
+import { UserRoles } from "../../types";
 
 type ProtectedRouteProps = {
     allowedRoles: UserRoles[]
     page: ReactNode
+    redirectTo: string
 }
 
-export const ProtectedRoute = ({ allowedRoles, page }: ProtectedRouteProps) => {
-
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+export const ProtectedRoute = ({ allowedRoles, page, redirectTo }: ProtectedRouteProps) => {
+    const [isAuthChecked, setIsAuthChecked] = useState<boolean>(false)
+    const isAuthorized = useAuthorization(allowedRoles)
     const navigate = useNavigate()
 
-    async function authenticate() {
-        const user = await Auth.currentAuthenticatedUser()
-        const userRoles = user?.signInUserSession?.accessToken?.payload['cognito:groups'] || []
-        console.log(userRoles)
-        if (allowedRoles.some(role => userRoles.includes(role))) setIsAuthenticated(true)
-        else navigate("/")
+    const checkAuthorization = async () => {
+        if (!isAuthorized && isAuthChecked) { navigate(redirectTo) }
     }
 
     useEffect(() => {
-        authenticate()
-    }, [])
+        if (!isAuthChecked) setIsAuthChecked(true)
+    }, [isAuthorized])
 
-    return isAuthenticated ? page : <></>
+    useEffect(() => {
+        checkAuthorization()
+    }, [isAuthChecked])
+
+    return isAuthorized ? page : <></>
 }
