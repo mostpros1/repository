68865345796cf80411b0e5c616258b2a1cf08dfc
Robin Zhao <titanen.1987@hotmail.com>
2024-01-30 17:09:39 +0100

    add user data to the dashboard

---
+++ web/src/pages/ProfessionalDashboard/ProfessionalDashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../../components/ui/NavBar/NavBar'
+import Footer from '../../components/ui/Footer/Footer'
+
+function ProfessionalDashboard() {
+    return (
+        <>
+            <NavBar />
+            <div>ProfessionalDashboard</div>
+            <Footer />
+        </>
+
+    )
+}
+export default ProfessionalDashboard
