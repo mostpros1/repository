3126f4958f8d71f99ea443f0c47a51faeb61251a
Bekoo023 <162413@novacollege.nl>
2024-02-07 15:56:21 +0100

    Kalender bijna af alleen moet ik nog de dagen en weken omdraaien naar de juiste plek

--- web/src/components/MultistepForm/Calendar.tsx
+++ web/src/components/MultistepForm/Calendar.tsx
@@ -1,94 +1,98 @@
 import React, { useState } from 'react';
-import { ToggleButton, Button, Grid, Typography } from '@mui/material';
+import './DatePicker.css'; // Aanname dat je CSS hierin staat
 
-function AvailabilitySelector() {
-    const [selectedDays, setSelectedDays] = useState({});
-    const daysOfWeek = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag'];
+const DateAndTimePicker: React.FC = () => {
+  const today = new Date();
+  const [date, setDate] = useState(today);
+  const currentMonth = date.getMonth();
+  const currentYear = date.getFullYear();
+  const [selectedDates, setSelectedDates] = useState<number[]>([]);
 
-    const handleDayToggle = (day) => {
-        setSelectedDays(prev => {
-            if (prev.hasOwnProperty(day)) {
-                const newState = { ...prev };
-                delete newState[day]; // Verwijder de gedeselecteerde dag
-                return newState;
-            } else {
-                // Initialiseer met start- en eindtijden
-                return { ...prev, [day]: { start: '', end: '' } };
-            }
-        });
-    };
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
 
-    const handleTimeChange = (day, type, event) => {
-        const newTime = event.target.value;
-        setSelectedDays(prev => ({
-            ...prev,
-            [day]: { ...prev[day], [type]: newTime }
-        }));
-    };
+  const handleDateSelect = (day: number, date: Date) => {
+    if (date < today) return; // Voorkom selectie van verleden dagen
+    setSelectedDates((prevDates) => {
+      return prevDates.includes(day) ? prevDates.filter(d => d !== day) : [...prevDates, day];
+    });
+  };
 
-    return (
-        <Grid container spacing={2} direction="column">
-            <Grid item>
-                <Typography variant="h4">Selecteer uw beschikbare dagen en tijden</Typography>
-                <Grid container spacing={1}>
-                    {daysOfWeek.map(day => (
-                        <Grid key={day} item sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <ToggleButton
-                                value={day}
-                                selected={selectedDays.hasOwnProperty(day)}
-                                onChange={() => handleDayToggle(day)}
-                                sx={{
-                                    height: '36px', 
-                                    fontSize: '15px',
-                                    fontWeight: 'bold',
-                                    width: '110px',
-                                    marginBottom: '5px',
-                                    marginTop: '20px',
-                                    transition: 'ease 0.5s',
-                                    border: 'solid 3px rgb(18, 114, 232)',
-                                    backgroundColor: selectedDays.hasOwnProperty(day) ? 'rgb(18, 114, 232)' : 'rgb(18, 114, 232)',
-                                    color: 'white',
-                                    '&:hover': {
-                                        backgroundColor: 'white',
-                                        color: 'black',
-                                    },
-                                    '&.Mui-selected': {
-                                        backgroundColor: 'white',
-                                        color: 'black',
-                                    },
-                                    '&.Mui-selected:hover': {
-                                        backgroundColor: 'rgb(18, 114, 232)',
-                                        color: 'white',
-                                        border: '0',
-                                    }
-                                }}
-                            >
-                                {day}
-                            </ToggleButton>
-                            {selectedDays.hasOwnProperty(day) && (
-                                <>
-                                    <p>Vanaf:</p>
-                                    <input
-                                        type="time"
-                                        value={selectedDays[day].start}
-                                        onChange={(e) => handleTimeChange(day, 'start', e)}
-                                        style={{ width: '100px', fontSize: '14px', margin: '5px 0' }}
-                                    />
-                                    <p>Tot:</p>
-                                    <input
-                                        type="time"
-                                        value={selectedDays[day].end}
-                                        onChange={(e) => handleTimeChange(day, 'end', e)}
-                                        style={{ width: '100px', fontSize: '14px', margin: '5px 0' }}
-                                    />
-                                </>
-                            )}
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
-        </Grid>
-    );
-}
+  const handlePrevMonth = () => {
+    setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
+  };
 
-export default AvailabilitySelector;
+  const handleNextMonth = () => {
+    setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
+  };
+
+  // Functie om het weeknummer te berekenen
+  const getWeekNumber = (d: Date) => {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    const weekNo = Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+    return weekNo;
+  };
+
+  // Genereer de dagen voor de gekozen maand
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  const renderCalendar = () => {
+    
+    let weeks: JSX.Element[][] = [];
+    let weekDays: JSX.Element[] = [];
+    let weekStartDates: Date[] = []; // Opslaan van de startdatum van elke week
+  
+    for (let day = 1; day <= daysInMonth; day++) {
+      const dayDate = new Date(currentYear, currentMonth, day);
+      const weekDay = dayDate.getDay();
+      const isMonday = weekDay === 1 || day === 1;
+  
+      if (isMonday) {
+        if (weekDays.length > 0) {
+          weeks.push([...weekDays]);
+          weekDays = [];
+        }
+        weekStartDates.push(dayDate); // Sla de startdatum van de nieuwe week op
+      }
+  
+      weekDays.push(<div key={day} className="day">{day}</div>);
+  
+      if (day === daysInMonth && weekDays.length > 0) {
+        weeks.push([...weekDays]);
+      }
+    }
+  
+    return weeks.map((week, index) => (
+      <div key={index} className="week">
+        <div className="week-number">{getWeekNumber(weekStartDates[index])}</div>
+        {week}
+      </div>
+    ));
+  };
+
+  return (
+    <div className="date-time-picker">
+      <div className="calendar">
+        <div className="month-selector">
+          <button onClick={handlePrevMonth} className='prev-month'>&lt;</button>
+          <span>{months[currentMonth]} {currentYear}</span>
+          <button onClick={handleNextMonth} className='next-month'>&gt;</button>
+        </div>
+        <div className="week-days">
+          {['Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'Zo'].map(day => (
+            <div key={day} className="week-day">{day}</div>
+          ))}
+        </div>
+        <div className="weeks">
+          {renderCalendar()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default DateAndTimePicker;
