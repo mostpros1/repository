1c5e722e34f236b526040a038aa2e68ea850ca21
Jasmeet Singh <jasmeet.singh@student.rocva.nl>
2024-02-12 14:53:03 +0100

    Update FilterBar.tsx

--- web/src/components/FilterBar/FilterBar.tsx
+++ web/src/components/FilterBar/FilterBar.tsx
@@ -3,10 +3,20 @@ import FilterDropdown from './FilterDropdown';
 import "./FilterBar.css";
 
 function FilterBar() {
+<<<<<<< Updated upstream
     // Sample data for filter options
     const locationOptions = ['All Locations', 'Amsterdam', 'Rotterdam', 'Haarlem'];
     const sortOptions = ['All Sorting', 'Van laag naar hoog', 'Van hoog naar laag'];
     const priceOptions = ['All Prices', '€500', '€1000', '€1500'];
+=======
+  const [showLocationOptions, setShowLocationOptions] = useState(false);
+  const [showSortOptions, setShowSortOptions] = useState(false);
+  const [showPriceOptions, setShowPriceOptions] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState("Select a location");
+  const [selectedSort, setSelectedSort] = useState("Select a sorting optio");
+  const [selectedPrice, setSelectedPrice] = useState("Select a price option");
+  const [itemsList, setItemsList] = useState(joblisting); // Gebruik de items van het geïmporteerde bestand
+>>>>>>> Stashed changes
 
     return (
         <div className="filterbar">
