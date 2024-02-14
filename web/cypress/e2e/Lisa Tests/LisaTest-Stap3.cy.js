c1d61c6a89b0d0f2f8ece460b6b542dbdfef79ea
Tarik-Bag <tarikihsan55@gmail.com>
2024-02-14 10:39:34 +0100

    Lisa test stap 4 optimalisatie
    Stap 4 isverbeterd.

--- web/cypress/e2e/Lisa Tests/LisaTest-Stap3.cy.js
+++ web/cypress/e2e/Lisa Tests/LisaTest-Stap3.cy.js
@@ -4,35 +4,36 @@ describe('Testing searchbar functionality', () => {
         function goBackAndClear() { // this is a special function, since 'textarea' is not an input field it cant be cleared with the goBack() func
             cy.get('[class="form-btn back"]').click();
 
-            cy.get('textarea').each(($input) => {
+            cy.get('.text-field').each(($input) => {
                 cy.wrap($input).clear();
             });
         }
 
-        cy.lisaTestBegin()
+        // Function to perform the checks in STAP 3
+        const performChecksStap3 = (textValues) => {
+            textValues.forEach((text) => {
+                cy.get('.text-field').type(text);
+                cy.goForward();
+                goBackAndClear();
+            });
+        };
 
-        cy.get('input[type="text"]').eq(0).type('2020TE')
-        cy.get('input[type="text"]').eq(1).type('Amsterdam')
-        cy.goForward()
-        cy.goForward()
 
-        // STAP 3
+        cy.lisaTestBegin();
+        cy.get('input[type="text"]').eq(0).type('2020TE');
+        cy.get('input[type="text"]').eq(1).type('Amsterdam');
+        cy.goForward();
+        cy.get('.dateCards_wrapper > :nth-child(4)').click()
+        cy.goForward();
 
-        // check 1
-        cy.get('[class="text-field"]').type('Ik heb een probleem.')
-        cy.goForward()
+        // Check 1-2
+        const textValuesStap3 = [
+            'Ik heb een probleem.',
+            'ŞÖĞİÜ(%/+^^+İŞÖ',
+            // Add more text values for additional checks
+        ];
 
-        goBackAndClear()
-
-        // check 2
-        cy.get('[class="text-field"]').type('ŞÖĞİÜ(%/+^^+İŞÖ')
-        cy.goForward()
-
-        goBackAndClear()
-
-        // check 3
-        cy.get('[class="text-field"]')
-        cy.goForward()
+        performChecksStap3(textValuesStap3);
 
     })
 })
--- web/cypress/e2e/Lisa Tests/LisaTest-Stap4.cy.js
+++ web/cypress/e2e/Lisa Tests/LisaTest-Stap4.cy.js
@@ -2,67 +2,56 @@ describe('Testing searchbar functionality', () => {
     it('type in input', () => {
 
         function lisaStap4TestStart() {
-            cy.lisaTestBegin()
-
-            cy.get('input[type="text"]').eq(0).type('2020TE')
-            cy.get('input[type="text"]').eq(1).type('Amsterdam')
-            cy.goForward()
-            cy.goForward()
-            cy.goForward()
+            cy.lisaTestBegin();
+            cy.get('input[type="text"]').eq(0).type('2020TE');
+            cy.get('input[type="text"]').eq(1).type('Amsterdam');
+            cy.goForward();
+            cy.get('.dateCards_wrapper > :nth-child(4)').click()
+            cy.goForward();
+            cy.goForward();
         }
 
-        lisaStap4TestStart()
+        function fillFormAndNavigateForward(text1, text2, email, tel, password1, password2) {
+            cy.get('input[type="text"]').eq(0).type(text1);
+            cy.get('input[type="text"]').eq(1).type(text2);
+            cy.get('input[type="email"]').eq(0).type(email);
+            cy.get('input[type="tel"]').eq(0).type(tel);
+            cy.get('input[type="password"]').eq(0).type(password1);
+            cy.get('input[type="password"]').eq(1).type(password2);
+            cy.goForward();
+        }
+
+        function clearAllInputs() {
+            cy.get('input').each(($input) => {
+                cy.wrap($input).clear();
+            });
+        }
+
+        lisaStap4TestStart();
 
         // STAP 4
 
-        // check 1
-        cy.get('input[type="text"]').eq(0).type('Test')
-        cy.get('input[type="text"]').eq(1).type('Test')
-        cy.get('input[type="email"]').eq(0).type('test@test.nl')
-        cy.get('input[type="tel"]').eq(0).type('+3109876323')
-        cy.get('input[type="password"]').eq(0).type('qwertyasdfg')
-        cy.get('input[type="password"]').eq(1).type('qwertyasdfg')
-        cy.goForward()
+        // Check 1
+        fillFormAndNavigateForward('Test', 'Test', 'test@test.nl', '+3109876323', 'qwertyasdfg', 'qwertyasdfg');
 
-        lisaStap4TestStart()
+        lisaStap4TestStart();
 
-        // check 2
-        cy.goForward()
+        // Check 2
+        cy.goForward();
 
-        // check 3-4-5-6
-        cy.get('input[type="text"]').eq(0).type('%&&^5İÜÇ')
-        cy.get('input[type="text"]').eq(1).type('%&(%^+İŞÇÖ')
-        cy.get('input[type="email"]').eq(0).type('%&$Y^@ğÜŞÇ:.^+%^+^')
-        cy.get('input[type="tel"]').eq(0).type('+/&/&%&ĞÜİŞÇÇiüi%+')
-        cy.get('input[type="password"]').eq(0).type('+%&/()=/&%+')
-        cy.get('input[type="password"]').eq(1).type('^^+%&/()/&%+')
-        cy.goForward()
+        // Check 3-4-5-6
+        fillFormAndNavigateForward('%&&^5İÜÇ', '%&(%^+İŞÇÖ', '%&$Y^@ğÜŞÇ:.^+%^+^', '+/&/&%&ĞÜİŞÇÇiüi%+', '+%&/()=/&%+', '^^+%&/()/&%+');
 
-        cy.get('input').each(($input) => {
-            cy.wrap($input).clear();
-        });
+        clearAllInputs();
 
-        // check 7
-        cy.get('input[type="text"]').eq(0).type('Test')
-        cy.get('input[type="text"]').eq(1).type('Test')
-        cy.get('input[type="email"]').eq(0).type('test@test.nl')
-        cy.get('input[type="tel"]').eq(0).type('+3109876323')
-        cy.get('input[type="password"]').eq(0).type('qwertyasdfg')
-        cy.get('input[type="password"]').eq(1).type('qwertyasdfgasdfg')
-        cy.goForward()
+        // Check 7
+        fillFormAndNavigateForward('Test', 'Test', 'test@test.nl', '+3109876323', 'qwertyasdfg', 'qwertyasdfgasdfg');
 
-        cy.get('input').each(($input) => {
-            cy.wrap($input).clear();
-        });
+        clearAllInputs();
 
-        // check 8
-        cy.get('input[type="text"]').eq(0).type('Test')
-        cy.get('input[type="text"]').eq(1).type('Test')
-        cy.get('input[type="email"]').eq(0).type('test@test.nl')
-        cy.get('input[type="tel"]').eq(0).type('+3109876323')
-        cy.get('input[type="password"]').eq(0).type('1234')
-        cy.get('input[type="password"]').eq(1).type('1234')
-        cy.goForward()
+        // Check 8
+        fillFormAndNavigateForward('Test', 'Test', 'test@test.nl', '+3109876323', '1234', '1234');
+
 
     })
 })
