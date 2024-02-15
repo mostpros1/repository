c45f1c3878a8ecd37885abde54ecb84c304a01c8
Tarik-Bag <tarikihsan55@gmail.com>
2024-02-15 12:14:16 +0100

    Vak Specialist Stap 1 optimalisatie

--- web/src/components/SpecialistMultistep/SearchChoreForm/SearchChoreForm.tsx
+++ web/src/components/SpecialistMultistep/SearchChoreForm/SearchChoreForm.tsx
@@ -1,4 +1,5 @@
 import "./SearchChoreForm.css"
+import { useState } from 'react';
 
 type SpecialistData = {
     beroep: string
@@ -12,6 +13,67 @@ type SearchChoreFormProps = SpecialistData & {
 }
 
 export default function SearchChoreForm({ beroep, email, postCode, stad, updateFields }: SearchChoreFormProps) {
+
+    const [isValidBeroep, setValidBeroep] = useState(true);
+
+    const handleBeroepChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValueBeroep = e.target.value;
+        const inputBeroepRegex = /^[A-Za-z\s]*$/; // Allow empty string
+        const isValidBeroep = inputBeroepRegex.test(inputValueBeroep);
+
+        setValidBeroep(isValidBeroep);
+
+        if (isValidBeroep || inputValueBeroep === '') {
+            updateFields({ beroep: inputValueBeroep });
+        }
+    };
+
+    const [isValidEmail, setValidEmail] = useState(true);
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValueEmail = e.target.value;
+        const inputEmailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
+        const isValidEmail = inputEmailRegex.test(inputValueEmail);
+
+        setValidEmail(isValidEmail);
+        updateFields({ email: inputValueEmail });
+    };
+
+
+    const [postcodeInput, setPostcodeInput] = useState(postCode);
+    const [isValidPostcode, setValidPostcode] = useState(true);
+
+    const [stadInput, setStadInput] = useState(stad);
+    const [isValidStad, setValidStad] = useState(true);
+
+    const handlePostcodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newPostcode = e.target.value;
+        const postcodeRegex = /^\d{4}\s?[A-Za-z]{2}$/;
+        const isValid = postcodeRegex.test(newPostcode);
+
+        setValidPostcode(isValid);
+
+        setPostcodeInput(newPostcode.slice(0, 6));
+
+        if (isValid || newPostcode === "") {
+            updateFields({ postCode: newPostcode.slice(0, 6) });
+        }
+    };
+
+    const handleStadChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newStad = e.target.value;
+        const stadRegex = /^[A-Za-z\s]+$/;
+        const isValidStad = stadRegex.test(newStad);
+
+        setValidStad(isValidStad);
+
+        setStadInput(newStad);
+
+        if (isValidStad || newStad === "") {
+            updateFields({ stad: newStad });
+        }
+    };
+
     return (
         <>
             <div className="search_chore_text_con">
@@ -22,32 +84,58 @@ export default function SearchChoreForm({ beroep, email, postCode, stad, updateF
                 <label>Uw hoofdberoep</label>
                 <input
                     type="text"
+                    required
                     placeholder="Uw beroep"
+                    className={`${isValidBeroep ? '' : 'invalid'}`}
                     value={beroep}
-                    onChange={e => updateFields({ beroep: e.target.value })}
+                    onChange={handleBeroepChange}
+                    pattern="[A-Za-z\s]+"
                 />
+                {!isValidBeroep && (
+                    <p className="error-message">Voer alstublieft een geldige beroep in</p>
+                )}
+
                 <label>Email:</label>
                 <input
-                    type="text"
+                    type="email"
+                    required
                     placeholder="example@example.com"
+                    pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$"
                     value={email}
-                    onChange={e => updateFields({ email: e.target.value })}
+                    onChange={handleEmailChange}
                 />
+                {!isValidEmail && (
+                    <p className="error-message">Voer alstublieft een geldig e-mailadres in</p>
+                )}
+
                 <label>Postcode</label>
                 <div className="search_chore_address">
                     <input
-                        type="text"
+                        type="postcode"
+                        required
                         placeholder="1234AB"
-                        value={postCode}
-                        onChange={e => updateFields({ postCode: e.target.value })}
+                        className={`${isValidPostcode ? '' : 'invalid'}`}
+                        value={postcodeInput}
+                        onChange={handlePostcodeChange}
+                        pattern="\d{4}\s?[A-Za-z]{2}"
                     />
                     <input
                         type="text"
+                        required
                         placeholder="Plaatsnaam"
-                        value={stad}
-                        onChange={e => updateFields({ stad: e.target.value })}
+                        className={`${isValidStad ? '' : 'invalid'}`}
+                        value={stadInput}
+                        onChange={handleStadChange}
+                        pattern="[A-Za-z\s]+"
                     />
                 </div>
+                {!isValidPostcode && (
+                    <p className="error-message">Voer alstublieft een geldige postcode in (bijv. 1234AB)</p>
+                )}
+
+                {!isValidStad && (
+                    <p className="error-message">Voer alstublieft een geldige stad in (bijv. Amsterdam)</p>
+                )}
                 <p className="form_login">Al een account? <a href="#">Inloggen</a></p>
             </div>
         </>
