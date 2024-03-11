b7b6a15b93b6a4c86967610fda12feaf83fb8270
Timon <timon.heidenreich@gmail.com>
2024-03-11 13:49:22 +0100

    test

--- Mostprosapp/app/index.tsx
+++ Mostprosapp/app/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { registerRootComponent } from "expo";
 import App from "./App";
 import { Amplify, Auth,} from "aws-amplify";
-import awsExports from "./aws-exports";
+import awsExports from "./aws-exports.js";
 Amplify.configure(awsExports);
 Auth.configure(awsExports);
 const Root = () => <App />;
