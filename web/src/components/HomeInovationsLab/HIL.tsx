e48746edc0282412d875b2610c70068c15155865
Jasmeet Singh <jasmeet.singh@student.rocva.nl>
2024-03-11 15:46:27 +0100

    Added a hero section with HIL logo and a background image.

--- web/src/components/HomeInovationsLab/HIL.tsx
+++ web/src/components/HomeInovationsLab/HIL.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import img from '../../assets/dutch_steamboat_small 2.jpg';
 import './HIL.css';
 
 function HIL() {
     return (
         <div id="root">
             <div className="main-content">
-                <h1>HomeInovation</h1>
+                <div className="image-container">
+                    <img src={img} alt="dutch steamboat" />
+                    <div className="text-overlay">
+                        <h1>Home Innovation Labs</h1>
+                        <p>Sustainable Value Creation</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- web/src/pages/HomeInovation/HomeInovation.tsx
+++ web/src/pages/HomeInovation/HomeInovation.tsx
@@ -1,14 +1,13 @@
 import NavBar from '../../components/ui/NavBar/NavBar'
 import Footer from '../../components/ui/Footer/Footer'
+import HIL from '../../components/HomeInovationsLab/HIL'
 import './HomeInovation.css'
 
 function HomeInovation() {
     return (
         <div id="root">
             <NavBar />
-            <div className="main-content">
-                <h1>HomeInovation</h1>
-            </div>
+            <HIL />
             <Footer />
         </div>
     )
