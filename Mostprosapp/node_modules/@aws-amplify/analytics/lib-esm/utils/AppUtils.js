8f61da7ffad4f4b9a1eac05abf9f048142e6e910
Timon <timon.heidenreich@gmail.com>
2024-03-12 15:07:40 +0100

    git ignore resetten

--- Mostprosapp/node_modules/@aws-amplify/analytics/lib-esm/utils/AppUtils.js
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-var isAppInForeground = function () {
-    return true;
-};
-export { isAppInForeground };
-//# sourceMappingURL=AppUtils.js.map
