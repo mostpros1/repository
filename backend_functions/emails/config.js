4330df34a10577a566a276dee60bf08bd243f58e
Timon <timon.heidenreich@gmail.com>
2024-03-12 15:10:45 +0100

    git ignore

---
+++ backend_functions/emails/config.js
@@ -0,0 +1,18 @@
+// config.js
+
+const config = {
+    aws: {
+        region: 'eu-north-1', // e.g., 'us-east-1'
+        ses: {
+            apiVersion: '2010-12-01',
+            /*accessKeyId: 'your-access-key-id',
+            secretAccessKey: 'your-secret-access-key',*/
+            // Set the default 'from' address for SES
+            from: {
+                default: 'timon@timonheidenreich.eu'
+            }
+        }
+    }
+};
+
+module.exports = config;
