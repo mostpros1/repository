cf3db6c381abe1dbd394d13ee9714d906c534052
Timon <timon.heidenreich@gmail.com>
2024-03-14 10:11:50 +0100

    bevestig email pagina hersteld en werkend gekregen

--- web/src/pages/BevestigEmailPage/BevestigEmailPage.tsx
+++ web/src/pages/BevestigEmailPage/BevestigEmailPage.tsx
@@ -21,7 +21,6 @@ function BevestigEmailPage() {
     const navigate = useNavigate()
     const inputRef = useRef([])
 
-    const userPassword = location.state === null ? "" : location.state.password
     const userEmail = location.state === null ? "" : location.state.email
     const postConfigId = location.state === null ? "" : location.state.postConfig
 
@@ -29,7 +28,7 @@ function BevestigEmailPage() {
         'HOMEOWNER': {
             roleName: "Homeowner",
             nextPage: '/huiseigenaar-resultaat',
-            onSuccess: () => setTimeout(() => navigate(postConfig.nextPage), 3000)
+            onSuccess: () => setTimeout(() => navigate(postConfigMap['HOMEOWNER'].nextPage), 3000)
         },
         'PROFESSIONAL': {
             roleName: "Professional",
@@ -40,70 +39,56 @@ function BevestigEmailPage() {
                     email: userEmail,
                     country: 'NL',
                 })
-                    .then(stripeAccount => {
-                        cognitoClient.adminUpdateUserAttributes({
-                            UserPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
-                            Username: userEmail,
-                            UserAttributes: [{
-                                Name: 'custom:stripeAccountId',
-                                Value: stripeAccount.id
-                            }]
-                        }).promise()
-                            .then(() => {
-                                stripeClient.accountLinks.create({
-                                    account: stripeAccount.id,
-                                    type: 'account_onboarding',
-                                    refresh_url: `${window.location.origin}/payments/onboarding-failed`,
-                                    return_url: `${window.location.origin}${postConfig.nextPage}`
-                                })
-                                    .then(result => window.location.href = result.url)
-                                    .catch(err => console.error(err))
-                            })
-                            .catch(err => console.error(err))
+                .then(stripeAccount => {
+                    cognitoClient.adminUpdateUserAttributes({
+                        UserPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
+                        Username: userEmail,
+                        UserAttributes: [{
+                            Name: 'custom:stripeAccountId',
+                            Value: stripeAccount.id
+                        }]
+                    }).promise()
+                    .then(() => {
+                        stripeClient.accountLinks.create({
+                            account: stripeAccount.id,
+                            type: 'account_onboarding',
+                            refresh_url: `${window.location.origin}/payments/onboarding-failed`,
+                            return_url: `${window.location.origin}${postConfigMap['PROFESSIONAL'].nextPage}`
+                        })
+                        .then(result => window.location.href = result.url)
+                        .catch(err => console.error(err))
                     })
                     .catch(err => console.error(err))
+                })
+                .catch(err => console.error(err))
             },
         }
     }
     const postConfig = postConfigMap[postConfigId] || null
 
-
     async function confirmSignUp(code: string) {
-
+        
         const confirmationResult = await Auth.confirmSignUp(userEmail, code)
-            .catch(error => {
-                console.error(error)
-                const errorActionMap: Record<string, () => void> = {
-                    "NotAuthorizedException": () => { setUserExists(true); setTimeout(() => navigate(postConfig.nextPage), 3000) },
-                    "CodeMismatchException": () => { },
-                    "default": () => { }
-                };
-                (errorActionMap[error.code] || errorActionMap['default'])()
-            })
+        .catch(error => {
+            console.error(error)
+            const errorActionMap: Record<string, () => void> = {
+                "NotAuthorizedException": () => { setUserExists(true); setTimeout(() => navigate(postConfigMap[postConfigId].nextPage), 3000) },
+                "CodeMismatchException": () => { },
+                "default": () => {}
+            };
+            (errorActionMap[error.code] || errorActionMap['default'])()
+        })
         const addToGroupResult = await cognitoClient.adminAddUserToGroup({
             UserPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
             Username: userEmail,
             GroupName: postConfig.roleName,
         }).promise()
-            .catch(error => console.error(error))
+        .catch(error => console.error(error))
         console.log(addToGroupResult)
         if (!addToGroupResult) return
         if (confirmationResult == 'SUCCESS') {
-            if (confirmationResult === 'SUCCESS' && addToGroupResult) {
-                setIsConfirmed(true);
-
-                // Sign in the user
-                /*const user = await Auth.signIn(userEmail, userPassword);
-                sessionStorage.setItem('accessToken', user.signInUserSession.accessToken.jwtToken);
-                sessionStorage.setItem('idToken', user.signInUserSession.idToken.jwtToken);
-                sessionStorage.setItem('refreshToken', user.signInUserSession.refreshToken.token);
-*/
-                // Perform any additional actions (if needed) upon successful sign-in
-                postConfig.onSuccess && postConfig.onSuccess();
-
-                // Navigate the user to the desired page
-                //navigate('/');
-            }
+            setIsConfirmed(true)
+            postConfig.onSuccess && postConfig.onSuccess()
         }
     }
 
@@ -117,47 +102,46 @@ function BevestigEmailPage() {
 
     function onNewCode() {
         Auth.resendSignUp(userEmail)
-            .catch(error => {
-                if (error.code == "InvalidParameterException") {
-                    setUserExists(true)
-                    setTimeout(() => navigate(postConfig.nextPage), 3000)
-                }
-            })
+        .catch(error => {
+            if (error.code == "InvalidParameterException") {
+                setUserExists(true)
+                setTimeout(() => navigate(postConfigMap[postConfigId].nextPage), 3000)
+            }
+        })
     }
     const form =
-        <div className="confirmemail-container">
-            <form className="confirmemail-card" onSubmit={onSubmit}>
-                <div className="confirmemail-card_header">
-                    <p className="confirmemail-card_title">Bevestig uw e-mailadres</p>
-                    <hr />
-                </div>
-                <b className="confirmemail-card_text">Er is een verificatiecode verzonden naar uw e-mailadres.</b>
-                <p className="confirmemail-card_text">Zoek in uw inbox naar een code, en voer die hieronder in. Let op: de mail kan mogelijk in uw spamfolder beland zijn.</p>
-                <DigitInputs amount={6} inputRef={inputRef} />
-                <button className="confirmemail-card_button" type="submit">Bevestigen</button>
-                <a className="confirmemail-card_newCode" onClick={onNewCode}>Nieuwe code versturen</a>
-            </form>
-        </div>
+    <div className="confirmemail-container">
+        <form className="confirmemail-card" onSubmit={onSubmit}>
+            <div className="confirmemail-card_header">
+                <p className="confirmemail-card_title">Bevestig uw e-mailadres</p>
+                <hr/>
+            </div>
+            <b className="confirmemail-card_text">Er is een verificatiecode verzonden naar uw e-mailadres.</b>
+            <p className="confirmemail-card_text">Zoek in uw inbox naar een code, en voer die hieronder in. Let op: de mail kan mogelijk in uw spamfolder beland zijn.</p>
+            <DigitInputs amount={6} inputRef={inputRef} />
+            <button className="confirmemail-card_button" type="submit">Bevestigen</button>
+            <a className="confirmemail-card_newCode" onClick={onNewCode}>Nieuwe code versturen</a>
+        </form>
+    </div>
 
     const confirmedPopup =
-        <div className="confirmemail-container confirmed">
-            <p className="confirmemail-card_title confirmed">Gelukt!</p>
-            <div className="confirmemail-screen confirmed">
-                <img src={ThumbsUp} alt="" />
-            </div>
-            <p className="confirmemail-card_text confirmed">Uw email is bevestigd. U wordt nu doorgestuurd naar de volgende pagina.</p>
+    <div className="confirmemail-container confirmed">
+        <p className="confirmemail-card_title confirmed">Gelukt!</p>
+        <div className="confirmemail-screen confirmed">
+            <img src={ThumbsUp} alt="" />
         </div>
+        <p className="confirmemail-card_text confirmed">Uw email is bevestigd. U wordt nu doorgestuurd naar de volgende pagina.</p>
+    </div>
 
     const userExistsPopup =
-        <div className="confirmemail-container confirmed">
-            <p className="confirmemail-card_title confirmed">U bent al geverifieerd.</p>
-            <p className="confirmemail-card_text confirmed">Uw account is al geverifieerd. U wordt nu doorgestuurd naar de volgende pagina.</p>
-        </div>
+    <div className="confirmemail-container confirmed">
+        <p className="confirmemail-card_title confirmed">U bent al geverifieerd.</p>
+        <p className="confirmemail-card_text confirmed">Uw account is al geverifieerd. U wordt nu doorgestuurd naar de volgende pagina.</p>
+    </div>
 
     return (
         userExists ? userExistsPopup : isConfirmed ? confirmedPopup : form
     )
 }
-
 
 export default BevestigEmailPage
