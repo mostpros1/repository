3402e1ec043d60c6cbbcb91a5f2dcaab73f8e5f0
Timon <timon.heidenreich@gmail.com>
2024-04-02 17:43:34 +0200

    json

--- backend_functions/declerations.ts
+++ backend_functions/declerations.ts
@@ -8,13 +8,13 @@ export const SES = new aws.SES({
 
 
 export const dynamoDB = new aws.DynamoDB({
-    region: process.env.VITE_AWS_REGION,
-    accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY
+    region: import.meta.env.VITE_AWS_REGION,
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
 });
 
 export const dynamo = new aws.DynamoDB.DocumentClient({
-    region: process.env.VITE_AWS_REGION,
-    accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY
+    region: import.meta.env.VITE_AWS_REGION,
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
 });
