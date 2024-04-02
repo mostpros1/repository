ec3ddee02f3319a663dd363b2383fd4eaede1f8a
Bekoo023 <162413@novacollege.nl>
2024-04-02 16:27:09 +0200

    fix acceptance

--- backend_functions/declerations.ts
+++ backend_functions/declerations.ts
@@ -1,19 +1,19 @@
-import aws from 'aws-sdk/clients/all';
+import aws from 'aws-sdk';
 
 export const ses = new aws.SES({
-    region: import.meta.env.VITE_AWS_REGION,
-    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+    region: process.env.VITE_AWS_REGION,
+    accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY
 });
 
 export const dynamoDB = new aws.DynamoDB({
-    region: import.meta.env.VITE_AWS_REGION,
-    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+    region: process.env.VITE_AWS_REGION,
+    accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY
 });
 
 export const dynamo = new aws.DynamoDB.DocumentClient({
-    region: import.meta.env.VITE_AWS_REGION,
-    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+    region: process.env.VITE_AWS_REGION,
+    accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY
 });
