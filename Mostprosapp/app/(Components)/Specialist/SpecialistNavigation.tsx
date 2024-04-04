10a503cf68c03255f63f7f00ca2cc18f119f9cdb
Tarik-Bag <tarikihsan55@gmail.com>
2024-04-04 13:55:07 +0200

    White space verwijderen en simple fix

--- Mostprosapp/app/(Components)/Specialist/SpecialistNavigation.tsx
+++ Mostprosapp/app/(Components)/Specialist/SpecialistNavigation.tsx
@@ -1,43 +1,42 @@
 import {
-    StyleSheet,
-    View,
-    SafeAreaView,
-    Text,
-    ImageBackground,
-    Pressable,
-  } from "react-native";
-  const SpecialistNavigation = ({navigation}) => {
-    return (
-      <SafeAreaView style={styles.view}>
-        <ImageBackground
-          source={require("../../../assets/images/welkomBackground.png")}
-          style={styles.backgroundImage}
-        >
-          <View style={styles.standardContainer}>
-            <View style={styles.sectionContainer}>
-              <Pressable style={styles.link} onPress={() => navigation.navigate('HomePageSpecialist')}>
-                  <Text style={[styles.text]}>Home Pagina</Text>
-              </Pressable>
-              <Pressable style={styles.link} onPress={() => navigation.navigate('OmgevingSpecialist')}>
-                <Text style={[styles.text]}>OmgevingProfessional</Text>
-              </Pressable>
-              <Pressable style={styles.link} onPress={() => navigation.navigate('DateAndTimePicker')}>
-                <Text style={[styles.text]}>DateAndTimePicker</Text>
-              </Pressable>
-              <Pressable style={styles.link} onPress={() => navigation.navigate('SpecialistResults')}>
-                <Text style={[styles.text]}>Result</Text>
-              </Pressable>
-              <Pressable style={styles.link} onPress={() => navigation.navigate('WorkNavigation')}>
-                <Text style={[styles.text]}>Klussen/ChangeDate navigatie</Text>
-              </Pressable>
-              <Pressable style={styles.link} onPress={() => navigation.navigate('PaymentSend')}>
-                <Text style={[styles.text]}>Payment Send</Text>
-              </Pressable>
-            </View>
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+const SpecialistNavigation = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.view}>
+      <ImageBackground
+        source={require("../../../assets/images/welkomBackground.png")}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.standardContainer}>
+          <View style={styles.sectionContainer}>
+            <Pressable style={styles.link} onPress={() => navigation.navigate('HomePageSpecialist')}>
+              <Text style={[styles.text]}>Home Pagina</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => navigation.navigate('OmgevingSpecialist')}>
+              <Text style={[styles.text]}>OmgevingProfessional</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => navigation.navigate('DateAndTimePicker')}>
+              <Text style={[styles.text]}>DateAndTimePicker</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => navigation.navigate('SpecialistResults')}>
+              <Text style={[styles.text]}>Result</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => navigation.navigate('WorkNavigation')}>
+              <Text style={[styles.text]}>Klussen/ChangeDate navigatie</Text>
+            </Pressable>
+            <Pressable style={styles.link} onPress={() => navigation.navigate('PaymentSend')}>
+              <Text style={[styles.text]}>Payment Send</Text>
+            </Pressable>
           </View>
         </View>
-      </ImageBackground>
-    </SafeAreaView>
+    </ImageBackground>
+    </SafeAreaView >
   );
 };
 
