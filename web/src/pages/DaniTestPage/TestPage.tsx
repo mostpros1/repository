d3b3173d0d52138f7acbd31a1f14b55cac583952
Danivdmijl <Danivandermijl@yahoo.com>
2024-04-04 15:28:07 +0200

    d

---
+++ web/src/pages/DaniTestPage/TestPage.tsx
@@ -0,0 +1,12 @@
+import "./TestPage.css";
+
+const TestPage = () => {
+    return (
+        <div className="maindiv">
+            <h1>Welcome to my website!</h1>
+            <p>This is the homepage content.</p>
+        </div>
+    );
+};
+
+export default TestPage;
