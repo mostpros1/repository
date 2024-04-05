42ed50b471d88501764a54061f8900eb095c5fd5
Danivdmijl <Danivandermijl@yahoo.com>
2024-04-05 12:40:26 +0200

    j

--- web/src/components/TestComp/TestComp.tsx
+++ web/src/components/TestComp/TestComp.tsx
@@ -1,10 +1,14 @@
 import "./TestComp.css";
 
-function TestComp(){
-    return(
+function TestComp() {
+    return (
         <main>
+            <section className="topButtonContainer">
+                <a className="topButton" href="https://www.danivandermijl.nl" target="_blank" rel="noopener noreferrer">Terug naar klussen</a>
+                <h2 className="pageTitle">Uw huidige klus: Loodgieter werk</h2>
+            </section>
         </main>
-    )
+    );
 }
 
 export default TestComp;
