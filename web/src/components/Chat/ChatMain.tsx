f6561bc4a49cf52679b3e426d7d5dc2b49bf4a6b
Bekoo023 <162413@novacollege.nl>
2024-04-11 14:49:13 +0200

    Update chat

--- web/src/components/Chat/ChatMain.tsx
+++ web/src/components/Chat/ChatMain.tsx
@@ -1,6 +1,6 @@
 import "@aws-amplify/ui-react/styles.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import * as mutations from "../../graphql/mutations";
 import { API, graphqlOperation } from "aws-amplify";
 import * as queries from "../../graphql/queries";
@@ -9,7 +9,7 @@ import * as subscriptions from "../../graphql/subscriptions";
 import { useChatBackend } from "./ChatBackend";
 import "./chatbox.css";
 
-function ChatMain({ user, signOut }) {
+function ChatMain({ user, signOut,}) {
   const {
     chats,
     setChats,
@@ -44,24 +44,32 @@ function ChatMain({ user, signOut }) {
     }
     fetchChats();
   }, [user.attributes.email]);
-
+  
   useEffect(() => {
     console.log("Checking for new messages...");
     const sub = API.graphql(
       graphqlOperation(subscriptions.onCreateChat)
       // @ts-ignore
-      ).subscribe({
-        next: ({ value }) => {
-          console.log("Received a new message:", value.data.onCreateChat);
-          handleReceivedMessage(value.data.onCreateChat);
-        },
-        error: (err) => console.log(err),
-      });
-      return () => sub.unsubscribe();
-    }, [user.attributes.email]);
-    
-    return (
-      <div className="chat-container">
+    ).subscribe({
+      next: ({ value }) => {
+        console.log("Received a new message:", value.data.onCreateChat);
+        handleReceivedMessage(value.data.onCreateChat);
+      },
+      error: (err) => console.log(err),
+    });
+    return () => sub.unsubscribe();
+  }, [user.attributes.email]);
+
+  const chatBoxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if(chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [chats]);
+  
+  return (
+    <div className="chat-container">
       <div className="sidebar" id="sidebar">
         {/* Sidebar content goes here */}
       </div>
@@ -73,55 +81,58 @@ function ChatMain({ user, signOut }) {
         </div>
       )}
     
-      <div className="button_container">
-        <button type="button" onClick={handleStartNewChat}>
+      <div className="button-container">
+        <button type="button" className="buttona" onClick={handleStartNewChat}>
           Start New Chat
         </button>
+        <button onClick={handleAlertConfirm} className="buttona">Confirm</button>
+        <button onClick={handleAlertCancel} className="buttona">Cancel</button>
+      {showAlert && (
+        <div className="alert">
+          <input
+            type="text"
+            placeholder="Enter recipient's email"
+            value={recipientEmail}
+            onChange={handleAlertInputChange}
+          />
+        </div>
+      )}
       </div>
     
       <div className="chat-main">
-        <div className="personlist">
-          {/* Person list goes here */}
+        <div className="chatheader">
+        <div className="chat-info">
+          <div className="name-and-status">
+            <h2 className="recipient-name">{recipientEmail}</h2>
+          </div>
         </div>
+      </div>
         
-        <div className="chat-box scrollable-chatbox">
+        <div className="chat-box scrollable-chatbox" ref={chatBoxRef}>
           {chats
-            .sort((a, b) => a.createdAt.localeCompare(b.createdAt))
-            .map((chat) => (
-              <div
-                key={chat.id}
-                className={`message-bubble ${
-                  chat.email === user.attributes.email ? "self-message" : "other-message"
-                }`}
-              >
-                {showAlert && (
-                  <div className="alert">
-                    <input
-                      type="text"
-                      placeholder="Enter recipient's email"
-                      value={recipientEmail}
-                      onChange={handleAlertInputChange}
-                    />
-                    <button onClick={handleAlertConfirm}>
-                      Confirm
-                    </button>
-                    <button onClick={handleAlertCancel}>Cancel</button>
-                  </div>
-                )}
-                
-                <div className="username">
-                  <span className="username-name">
-                    {chat.email.split("@")[0]}
-                  </span>
-                </div>
-                <p className="text">
-                  {chat.text}
-                </p>
-                  <time dateTime={chat.createdAt} className="time">
-                    {intlFormatDistance(new Date(chat.createdAt), new Date())}
-                  </time>
-              </div>
-            ))}
+          .sort((a, b) => a.createdAt.localeCompare(b.createdAt))
+          .map((chat) => (
+          <div
+            key={chat.id}
+            className={`message-container ${
+              chat.email === user.attributes.email ? "self-message-container" : "other-message-container"
+            }`}
+          >
+        <div
+          className={`message-bubble ${
+            chat.email === user.attributes.email ? "self-message" : "other-message"
+          }`}
+        >
+            <div className="username">
+              <span className="username-name">{chat.email.split("@")[0]}</span>
+            </div>
+            <p className="text">{chat.text}</p>
+          </div>
+          <time dateTime={chat.createdAt} className="message-time">
+            {`${intlFormatDistance(new Date(chat.createdAt), new Date())}`}
+          </time>
+        </div>
+        ))}
         </div>
         
         <div className="input-form">
