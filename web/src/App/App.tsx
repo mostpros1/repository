24f7db77d732b70d5869e5ae777bb437707e8273
Danivdmijl <Danivandermijl@yahoo.com>
2024-04-12 13:01:07 +0200

    Added start new Jobs page

--- web/src/App/App.tsx
+++ web/src/App/App.tsx
@@ -33,6 +33,7 @@ import DetailJobPage from "../pages/DetailJobPage/DetailJobPage";
 import MijnKlussenOverzichtPage from "../pages/MijnKlussenOverzichtPage/MijnKlussenOverzichtPage";
 import VSMijnklussen from "../pages/VakspecialistMijnKlussen/VSMijnklussen";
 import ConfirmDatePage from "../pages/ConfirmDatePage/ConfirmDatePage";
+import JobsPage from "../pages/JobsPage/JobsPage";
 import "./App.css";
 import ChatContactList from "../components/Chat/ChatContactList";
 
@@ -62,6 +63,7 @@ const App = () => {
       <Route path="/MijnKlussenOverzicht" element={<MijnKlussenOverzichtPage />} />
       <Route path="/OverOns" element={<OverOns />} />
       <Route path="/ConfirmDate" element={<ConfirmDatePage />} />
+      <Route path="/Jobs" element={<JobsPage />} />
 
 
       <Route path="/VSMijnklussen" element={<VSMijnklussen />} />
