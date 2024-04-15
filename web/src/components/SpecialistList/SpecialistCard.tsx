5140a0f7a7ec0b618f5764a1a8a9e60b2a5447f7
Timon <timon.heidenreich@gmail.com>
2024-04-15 18:51:37 +0200

    specialisten

--- web/src/components/SpecialistList/SpecialistCard.tsx
+++ web/src/components/SpecialistList/SpecialistCard.tsx
@@ -56,27 +56,27 @@ function SpecialistCard() {
   ];
 
   useEffect(() => {
-
     let professionals: any[] = [];
 
     const profession = window.location.hash.replace("#", "").split("?")[0];
-  
+
     const task = window.location.hash.replace("#", "").split("?")[1];
-  
-    dynamo
-      .query({
-        TableName: "Professionals",
-        IndexName: "profession",
-        KeyConditionExpression: "profession = :profession",
-        FilterExpression: "task = :task",
-        ExpressionAttributeValues: {
-          ":profession": profession,
-          ":task": task,
-        },
-      })
-      .promise()
-      .then((data) => {
-        const convertedItems = data.Items?.map((item) => ({
+
+    const date = window.location.hash.replace("#", "").split("!")[1];
+
+    dynamo.query({
+      TableName: "Professionals",
+      IndexName: "profession",
+      KeyConditionExpression: "profession = :profession",
+      FilterExpression: "task = :task",
+      ExpressionAttributeValues: {
+        ":profession": profession,
+        ":task": task,
+      },
+    }).promise()
+      .then(data => {
+
+        const convertedItems = data.Items?.map(item => ({
           id: item.id,
           first_name: item.first_name,
           last_name: item.last_name,
@@ -86,37 +86,38 @@ function SpecialistCard() {
           rating: item.rating,
           bio: item.bio,
           availibility: item.availibility,
+
+
         }));
         /*
-                  const Availability = JSON.parse(convertedItems[0].availibility);
-                  '{"dates":["2024-03-31","2024-04-01","2024-04-02"]}'
-                  console.log("Availability =", Availability);
-          */
-  
+                const Availability = JSON.parse(convertedItems[0].availibility);
+                '{"dates":["2024-03-31","2024-04-01","2024-04-02"]}'
+                console.log("Availability =", Availability);
+        */
+
         if (convertedItems) {
           for (let i: number = 0; i < convertedItems.length; i++) {
             if (convertedItems[i].availibility) {
-              /*const Availability = JSON.parse(convertedItems[i].availibility);
-  
+              const Availability = JSON.parse(convertedItems[i].availibility);
+
               for (let x: number = 0; x < Availability.dates.length; x++) {
-                const selected = JSON.stringify(updateDate)
-                  .replace("T", '"')
-                  .split('"')[3];
-  
+
+                const selected = JSON.stringify(date).replace('T', '"').split('"')[3];
+
                 if (selected == Availability.dates[x]) {
                   professionals = [...professionals, convertedItems[i]];
                   break;
                 }
-              }*/
-              professionals = [...professionals, convertedItems[i]];
+              }
             }
           }
-          setSpecialists(/*convertedItems*/ professionals);
-        }
-      })
-      .catch((err) => {
+        setSpecialists(/*convertedItems*/professionals);
+
+
+      }}).catch(err => {
         console.log(err);
       });
+
   }, []);
 
   let specialistCardRender = specialists.map((specialist) => {
