0d96f01d229770ce2dfae338f4832b0cae9121b1
Bekoo023 <162413@novacollege.nl>
2024-04-16 15:27:22 +0200

    delete chat in nav

--- web/src/components/ui/NavBar/Navigation.tsx
+++ web/src/components/ui/NavBar/Navigation.tsx
@@ -68,7 +68,6 @@ function Navigation() {
         <p>{user.attributes.email}</p>
         {DashboardLink}
         <button onClick={handleLogout}>Uitloggen</button>
-        <Link to="/chat">Chat</Link>
       </>
     );
   }
