bd865664fcc9cd983dc8c9cdd52dd7a734681944
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2024-04-19 16:28:30 +0200

    update

---
+++ web/declarations.ts
@@ -0,0 +1,23 @@
+import aws from "aws-sdk";
+export const cognitoIdentityServiceProvider = new aws.CognitoIdentityServiceProvider();
+
+export const SES = new aws.SES({
+    region: import.meta.env.VITE_AWS_REGION,
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+});
+
+export const dynamoDB = new aws.DynamoDB({
+    region: import.meta.env.VITE_AWS_REGION,
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+});
+
+export const dynamo = new aws.DynamoDB.DocumentClient({
+    region: import.meta.env.VITE_AWS_REGION,
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+});
+
+
+export const MostropsEmail = "timon@timonheidenreich.eu";
