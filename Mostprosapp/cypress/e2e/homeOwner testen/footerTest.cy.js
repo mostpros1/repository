948d826e6c3e1ac44e29bb6d275fc1fa966e026f
Tarik-Bag <tarikihsan55@gmail.com>
2024-04-23 15:42:51 +0200

    Test fixes
    Dit zijn de laatste versies van deze tests. Misschien is het mogelijk om dingen in de toekomst te verbeteren.

--- Mostprosapp/cypress/e2e/homeOwner testen/footerTest.cy.js
+++ Mostprosapp/cypress/e2e/homeOwner testen/footerTest.cy.js
@@ -9,17 +9,23 @@ describe('Home Owner Test: Footer Test', () => {
 
         cy.get('[data-testid="footer homeBtn"]').click()
 
+        // assertion
+        cy.get('[data-testid="searchbar"]').should('be.visible')
     })
 
     it('Footer test 2: Chat button test', () => {
 
         cy.get('[data-testid="footer chatOverviewBtn"]').click()
 
+        // assertion
+        cy.get('.r-gap-hnu01a > :nth-child(1)').should('be.visible')
     })
 
     it('Footer test 3: Profiel button test', () => {
 
         cy.get('[data-testid="footer profileBtn"]').click()
 
+        // assertion
+        cy.contains('Profiel Bijwerken').should('be.visible')
     })
 })
