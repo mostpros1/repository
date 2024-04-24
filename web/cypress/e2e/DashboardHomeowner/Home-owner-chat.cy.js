d42381eaded46cf31654f5c8ef327ad4cf8da51f
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2024-04-24 10:48:17 +0200

    new update

---
+++ web/cypress/e2e/DashboardHomeowner/Home-owner-chat.cy.js
@@ -0,0 +1,14 @@
+import faker from 'faker';
+
+// Stap 1
+context('Text in searchbaar typpen"', () => {
+  for (let i = 1; i < 2; i++) {
+    it(`Type in input - test ${i}`, () => {
+      cy.Jobs();
+      // TEST 1: Type in input
+      cy.get('input[type="text"]').eq(0).type(faker.name.jobType());
+      cy.chat();
+      cy.Jobs();
+    });
+  }
+});
--- web/cypress/e2e/DashboardHomeowner/Homeowner-job.cy.js
+++ web/cypress/e2e/DashboardHomeowner/Homeowner-job.cy.js
@@ -14,13 +14,14 @@ context('Text in searchbaar typpen"', () => {
 });
 
 // Stap 2
-context('Text in searchbaar typpen"', () => {
+context('Klikbaar van current jobs-Finished jobs"', () => {
     for (let i = 1; i < 2; i++) {
       it(`Type in input - test ${i}`, () => {
         cy.Jobs();
-        // TEST 1: Type in input
-        cy.get('input[type="text"]').eq(0).type(faker.name.jobType());
-        cy.get('[class="job-actions"]').click().should('be.visible').click();
+        // TEST 2: Klikbaar van current jobs-Finished jobs
+        cy.get('.job-status > .active').click();
+        cy.get('.job-status > :nth-child(2)').click();
+        cy.get('.job-status > .active').click();
         cy.chat();
         cy.Jobs();
       });
