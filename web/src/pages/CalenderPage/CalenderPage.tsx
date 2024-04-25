a4d6a21e825f4bf158cb2669879fa279653bc0c3
Jass <jasmeet.singh@student.rocva.nl>
2024-04-25 14:02:46 +0200

    Add NavBar, SideNav, and Footer components to CalenderPage

--- web/src/pages/CalenderPage/CalenderPage.tsx
+++ web/src/pages/CalenderPage/CalenderPage.tsx
@@ -1,11 +1,22 @@
 import Calender from "../../components/SpecialistMultistep/Calendar";
+import NavBar from "../../components/ui/NavBar/NavBar";
+import Footer from "../../components/ui/Footer/Footer";
+import SideNav from "../../components/ui/SideNav/SideNav";
+import "./CalenderPage.css";
 
 const CalenderPage = () => {
-    return (
-      <>
-        <Calender/>
-      </>
-    );
-  };
-  
-  export default CalenderPage;
+  return (
+    <>
+      <NavBar />
+      <div className="calendar-grid-con">
+        <div className="calendar-sidenav-con">
+          <SideNav />
+        </div>
+        <Calender />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default CalenderPage;
