d9f8fade2e64deeb924e7fdf9ad1b0ed49cacf50
Bekoo023 <162413@novacollege.nl>
2024-04-25 12:20:25 +0200

    fix

--- web/src/components/ui/SideNav/SideNav.tsx
+++ web/src/components/ui/SideNav/SideNav.tsx
@@ -39,28 +39,6 @@ function SideNav() {
   //       navigate("/login"); // Redirect to login or another appropriate page
   //     }
   //   }, 50); // Delay in milliseconds (5000ms = 5s)
-  const [IsHomeOwner, setIsHomeOwner] = useState(false);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (
-        user &&
-        user.signInUserSession &&
-        user.signInUserSession.accessToken &&
-        user.signInUserSession.accessToken.payload
-      ) {
-        const groups =
-          user.signInUserSession.accessToken.payload["cognito:groups"];
-        if (groups && groups.includes("Homeowner")) {
-          setIsHomeOwner(true);
-        } else if (groups && groups.includes("Professional")) {
-          setIsProfessional(true);
-        }
-      } else {
-        // Handle the case where the user data is not fully available
-        console.log("User data is not fully available.");
-        navigate("/login"); // Redirect to login or another appropriate page
-      }
-    }, 500); // Delay in milliseconds (5000ms = 5s)
 
   //   return () => clearTimeout(timer); // Clear the timeout if the component unmounts before the timeout is called
   // }, [user, navigate]); // Depend on user and navigate to ensure updates
