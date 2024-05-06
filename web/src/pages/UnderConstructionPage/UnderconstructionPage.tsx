1f4252396726b2824e467892006613d5987a6fd5
Jass Singh <2087558@talnet.nl>
2024-05-06 14:07:41 +0200

    Construction page added

---
+++ web/src/pages/UnderConstructionPage/UnderconstructionPage.tsx
@@ -0,0 +1,21 @@
+import NavBar from '../../components/ui/NavBar/NavBar';
+import Footer from '../../components/ui/Footer/Footer';
+import './UnderConstructionPage.css';
+
+
+function UnderConstructionPage() {
+    return (
+        <>
+            <NavBar />
+            <div className="UnderConstructionPage">
+                <div className="UnderConstructionPage-content">
+                    <h1>Mostpros</h1>
+                    <p>Sorry, this page is under construction.</p>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default UnderConstructionPage;
