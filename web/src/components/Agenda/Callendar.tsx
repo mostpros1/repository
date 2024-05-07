25b725bc6b4f074b404340b9ebfed3d1f5e105b8
Timon <timon.heidenreich@gmail.com>
2024-05-07 17:53:21 +0200

    calender

--- web/src/components/Agenda/Callendar.tsx
+++ web/src/components/Agenda/Callendar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import SideNav from "../../ui/SideNav/SideNav.tsx";
-import Navigation from "../../ui/NavBar/Navigation.tsx";
-import Footer from "../../ui/Footer/Footer.tsx";
+import SideNav from "../ui/SideNav/SideNav.tsx";
+import Navigation from "../ui/NavBar/Navigation.tsx";
+import Footer from "../ui/Footer/Footer.tsx";
 import Cal from "./cal.tsx";
 import './Callendar.css'; // Import the CSS file
-import "../../ui/NavBar/NavBar.css";
+import "../ui/NavBar/NavBar.css";
 
 function Callendar() {
     return (
