980d8d784894a5ab11537359851a5b2ddffcab14
Timon <timon.heidenreich@gmail.com>
2024-05-07 21:53:27 +0200

    dingen weg haalen

--- web/src/pages/CalenderPage/CalenderPage.tsx
+++ web/src/pages/CalenderPage/CalenderPage.tsx
@@ -1,20 +1,10 @@
 import Cal from "../../components/Agenda/Callendar";
-import NavBar from "../../components/ui/NavBar/NavBar";
-import Footer from "../../components/ui/Footer/Footer";
-import SideNav from "../../components/ui/SideNav/SideNav";
 import "./CalenderPage.css";
 
 const CalenderPage = () => {
   return (
     <>
-      <NavBar />
-      <div className="calendar-panel">
-        <div className="side-panel-left-calendar">
-          <SideNav />
-        </div>
         <Cal />
-      </div>
-      <Footer />
     </>
   );
 };
