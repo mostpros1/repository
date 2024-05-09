1c29c0e4c3b1169ce7d146d627e54fd1189ddd82
Timon <timon.heidenreich@gmail.com>
2024-05-09 16:35:50 +0200

    dropdown

--- web/src/components/Agenda/cal.tsx
+++ web/src/components/Agenda/cal.tsx
@@ -2,66 +2,94 @@ import React, { useState } from 'react';
 import { format, startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays, subMonths, addMonths } from 'date-fns';
 import { nl } from 'date-fns/locale';
 import styled from 'styled-components';
-import arrowL from './arrowL.png'; // Pas het pad aan naar nodig
-import arrowR from './arrowR.png'; // Pas het pad aan naar nodig
+import arrowL from './arrowL.png'; // Adjust the path as needed
+import arrowR from './arrowR.png'; // Adjust the path as needed
 import { Auth } from 'aws-amplify';
 import { dynamo } from '../../../declarations';
 
 const CalendarContainer = styled.div`
- display: grid;
- grid-template-columns: repeat(7, 1fr);
- gap: 1px;
- background-color: #f5f5f5;
- width: 100%; // Zorg ervoor dat de kalender de volledige breedte beslaat
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    gap: 1px;
+    background-color: #f5f5f5;
+    width: 100%; 
 `;
 
 interface DayProps {
     isCurrentMonth: boolean;
-    isPreviousMonth: boolean; // Toegevoegd
-    isNextMonth: boolean; // Toegevoegd
-    onClick: () => void; // Add this line
-    hasEntries: boolean; // Add this line to indicate if there are entries
+    isPreviousMonth: boolean; 
+    isNextMonth: boolean; 
+    onClick: () => void; 
+    hasEntries: boolean; 
 }
 
 const Day = styled.button<DayProps>`
- padding: 20px; // Verhoog de padding om de dagvakken groter te maken
- background-color: ${props => props.isCurrentMonth ? 'white' : '#e0e0e0'};
- border: 1px solid #e0e0e0;
- color: ${props => props.isCurrentMonth ? 'black' : 'gray'};
- cursor: pointer; // Make the cursor change to a pointer when hovering over a day
- position: relative; // Allow positioning of child elements
- &::after {
-     content: '';
-     position: absolute;
-     bottom: 0;
-     left: 50%;
-     transform: translateX(-50%);
-     width: 10px;
-     height: 10px;
-     border-radius: 50%;
-     background-color: ${props => props.hasEntries ? 'red' : 'transparent'}; // Change color as needed
- }
+    position: relative;
+    padding: 20px;
+    background-color: ${props => props.isCurrentMonth ? 'white' : '#e0e0e0'};
+    border: 1px solid #e0e0e0;
+    color: ${props => props.isCurrentMonth ? 'black' : 'gray'};
+    cursor: pointer;
+    &::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background-color: ${props => props.hasEntries ? 'red' : 'transparent'};
+    }
+    .dropdown {
+        display: none;
+        position: absolute;
+        top: calc(100% + 5px);
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: white;
+        border: 1px solid #ccc;
+        padding: 10px;
+        z-index: 1;
+    }
+    &:hover .dropdown {
+        display: block;
+    }
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const CheckboxLabel = styled.label`
+    margin-bottom: 5px;
+`;
+
+const Checkbox = styled.input`
+    margin-right: 5px;
 `;
 
 const DaysOfWeek = styled.div`
- display: flex;
- justify-content: space-around;
- background-color: #178feb;
- color: white;
- padding: 10px;
+    display: flex;
+    justify-content: space-around;
+    background-color: #178feb;
+    color: white;
+    padding: 10px;
 `;
 
 const MonthYearDisplay = styled.div`
- text-align: center;
- font-size: 20px;
- font-weight: bold;
- padding: 10px;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    padding: 10px;
 `;
 
 const ButtonContainer = styled.div`
- display: flex;
- justify-content: space-between;
- padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
 `;
 
 interface NavButtonProps {
@@ -70,14 +98,14 @@ interface NavButtonProps {
 }
 
 const NavButton = styled.button<NavButtonProps>`
-     background: none;
-     border: none;
-     cursor: pointer;
-     width: 30px;
-     height: 30px;
-     background-image: url(${props => props.image});
-     background-size: cover;
-   `;
+    background: none;
+    border: none;
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+    background-image: url(${props => props.image});
+    background-size: cover;
+`;
 
 const Cal = () => {
     const [currentMonth, setCurrentMonth] = useState(new Date());
@@ -88,7 +116,6 @@ const Cal = () => {
         setSelectedDate(date);
     };
 
-
     async function getEntriesFromDB() {
         const authenticatedUser = await Auth.currentAuthenticatedUser();
         const email = authenticatedUser.attributes.email;
@@ -97,7 +124,7 @@ const Cal = () => {
             IndexName: "emailIndex",
             KeyConditionExpression: "email = :email",
             ExpressionAttributeValues: {
-                ":email": email // Use the email variable here
+                ":email": email 
             },
         }).promise().then((data) => {
             if (data.Items && data.Items.length > 0) {
@@ -112,7 +139,6 @@ const Cal = () => {
 
     const addEntry = (entry: string, time: string, color: string) => {
         if (selectedDate === null) {
-            // Handle the case where selectedDate is null, e.g., by logging an error or setting a default date
             console.error("Selected date is null. Cannot add entry.");
             return;
         }
@@ -124,7 +150,7 @@ const Cal = () => {
     };
 
     const renderDaysOfWeek = () => {
-        const date = new Date(1970, 0, 4); // 4 januari 1970, zondag
+        const date = new Date(1970, 0, 4);
         const days: React.ReactElement[] = [];
 
         for (let i = 0; i < 7; i++) {
@@ -155,6 +181,20 @@ const Cal = () => {
             days.push(
                 <Day key={d.getTime()} isCurrentMonth={isCurrentMonth} isPreviousMonth={isPreviousMonth} isNextMonth={isNextMonth} onClick={() => handleDayClick(d)} hasEntries={hasEntries}>
                     {format(d, 'd')}
+                    <div className="dropdown">
+                        <Form>
+                            <CheckboxLabel>
+                                <Checkbox type="checkbox" name="option1" onChange={() => {}} />
+                                Option 1
+                            </CheckboxLabel>
+                            <CheckboxLabel>
+                                <Checkbox type="checkbox" name="option2" onChange={() => {}} />
+                                Option 2
+                            </CheckboxLabel>
+                            {/* Add more checkboxes as needed */}
+                            <button type="submit">Submit</button>
+                        </Form>
+                    </div>
                 </Day>
             );
         }
@@ -176,7 +216,6 @@ const Cal = () => {
 
     const addAvailibility = async (date: string, time: string, color: string) => {
         if (selectedDate === null) {
-            // Handle the case where selectedDate is null, e.g., by logging an error or setting a default date
             console.error("Selected date is null. Cannot add entry.");
             return;
         }
@@ -235,7 +274,7 @@ const Cal = () => {
             </ButtonContainer>
             {renderDaysOfWeek()}
             {renderCalendarDays()}
-            < button onClick={goToCurrentMonth} > Ga naar huidige maand</button >
+            <button onClick={goToCurrentMonth}>Go to current month</button>
             {selectedDate && (
                 <div>
                     <h3>Entries for {format(selectedDate, 'yyyy-MM-dd')}</h3>
@@ -245,39 +284,43 @@ const Cal = () => {
                                 <div style={{ backgroundColor: entry.color, opacity: 0.5, padding: '5px', margin: '2px 0', borderRadius: '5px' }}>
                                     <>
                                         {entry.text}
-                                        {" "}
+                                        {' '}
                                         {entry.time}
                                     </>
                                 </div>
                             </li>
                         ))}
                     </ul>
-                    <form onSubmit={e => {
-                        e.preventDefault();
-                        const entry = (e.target as HTMLFormElement).elements.namedItem('entry') as HTMLInputElement;
-                        const color = (e.target as HTMLFormElement).elements.namedItem('color') as HTMLSelectElement;
-                        addEntry(entry.value, "", color.value);
-                        entry.value = '';
-                    }}>
+                    <form
+                        onSubmit={e => {
+                            e.preventDefault();
+                            const entry = (e.target as HTMLFormElement).elements.namedItem('entry') as HTMLInputElement;
+                            const color = (e.target as HTMLFormElement).elements.namedItem('color') as HTMLSelectElement;
+                            addEntry(entry.value, '', color.value);
+                            entry.value = '';
+                        }}
+                    >
                         <input type="text" name="entry" placeholder="Add an entry" />
                         <select name="color">
-                            <option value="rgba(0,0,0,0.5)">Zwart</option>
-                            <option value="rgba(255,0,0,0.5)">Rood</option>
-                            <option value="rgba(0,255,0,0.5)">Groen</option>
-                            <option value="rgb(52, 143, 255)">Blauw</option>
-                            {/* Add more colors as needed */}
+                            <option value="rgba(0,0,0,0.5)">Black</option>
+                            <option value="rgba(255,0,0,0.5)">Red</option>
+                            <option value="rgba(0,255,0,0.5)">Green</option>
+                            <option value="rgb(52, 143, 255)">Blue</option>
                         </select>
                         <button type="submit">Add</button>
                     </form>
-                    <form onSubmit={e => {
-                        e.preventDefault();
-                        const date = (e.target as HTMLFormElement).elements.namedItem('date') as HTMLInputElement;
-                        const time = (e.target as HTMLFormElement).elements.namedItem('time') as HTMLInputElement;
-                        addAvailibility(date.value, time.value, "");
-                        date.value = '';
-                        time.value = '';
-                    }}>
-                        <b>Voeg beschikbaarheid toe</b><br></br>
+                    <form
+                        onSubmit={e => {
+                            e.preventDefault();
+                            const date = (e.target as HTMLFormElement).elements.namedItem('date') as HTMLInputElement;
+                            const time = (e.target as HTMLFormElement).elements.namedItem('time') as HTMLInputElement;
+                            addAvailibility(date.value, time.value, '');
+                            date.value = '';
+                            time.value = '';
+                        }}
+                    >
+                        <b>Add availability</b>
+                        <br></br>
                         <input type="date" name="date" />
                         <input type="time" name="time" />
                         <button type="submit">Add</button>
