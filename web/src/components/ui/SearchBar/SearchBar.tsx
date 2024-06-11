4835dab5bab37beee55bd60a7dc45fb4335e0313
Jass Singh <2087558@talnet.nl>
2024-06-11 11:11:08 +0200

    chore: Update NavBar to handle dashboard URL and user groups

--- web/src/components/ui/SearchBar/SearchBar.tsx
+++ web/src/components/ui/SearchBar/SearchBar.tsx
@@ -1,143 +1,143 @@
-import "./SearchBar.css";
-import specialists from "../../../data/specialists.js";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import "./SearchBar.css";
+// import specialists from "../../../data/specialists.js";
+// import { Link } from "react-router-dom";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-// ...
+// // ...
 
-interface Specialist {
-  id: number;
-  name: string;
-  tasks: { task: string; link: string }[];
-  link?: string;
-}
+// interface Specialist {
+//   id: number;
+//   name: string;
+//   tasks: { task: string; link: string }[];
+//   link?: string;
+// }
 
-function Searchbar() {
-  const [value, setValue] = useState("");
-  const [showList, setShowList] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+// function Searchbar() {
+//   const [value, setValue] = useState("");
+//   const [showList, setShowList] = useState(false);
+//   const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  const handleInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (
-      !e.relatedTarget ||
-      !e.relatedTarget.classList.contains("search_dropdown_item")
-    ) {
-      setShowList(false);
-    }
-  };
+//   const handleInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+//     if (
+//       !e.relatedTarget ||
+//       !e.relatedTarget.classList.contains("search_dropdown_item")
+//     ) {
+//       setShowList(false);
+//     }
+//   };
 
-  const navigate = useNavigate();
-  const handleInputFocus = () => {
-    setShowList(true);
-  };
-  const handleResultClick = (link: string) => {
-    navigate(`/nl/jobs${link}`);
-  };
+//   const navigate = useNavigate();
+//   const handleInputFocus = () => {
+//     setShowList(true);
+//   };
+//   const handleResultClick = (link: string) => {
+//     navigate(`/nl/jobs${link}`);
+//   };
 
 
-  const handleInputKeyDown = (e) => {
-    switch (e.key) {
-      case "ArrowUp":
-        setSelectedIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-        break;
-      case "ArrowDown":
-        setSelectedIndex((prevIndex) =>
-          Math.min(prevIndex + 1, slicedResults.length - 1)
-        );
-        break;
-      case "Enter":
-        if (selectedIndex >= 0 && selectedIndex < slicedResults.length) {
-          const selectedResult = slicedResults[selectedIndex];
-          handleResultClick(selectedResult.link);
-        }
-        break;
-      case "Tab": // Implementing autocomplete on Tab key
-        if (slicedResults.length > 0) {
-          const selectedResult = slicedResults[0];
-          setValue(selectedResult.task); // Autocomplete with the first suggestion
-          setSelectedIndex(0);
-        }
-        break;
-      default:
-        break;
-    }
-  };
-
-  
-  const searchResults = () => {
-    const searchTerm = value.toLowerCase().trim();
-
-    // Search for matches in individual tasks and specialist names
-    const taskResults = specialists.flatMap((specialist) => {
-      const tasks = specialist.tasks
-        .filter((task) => task.task.toLowerCase().includes(searchTerm))
-        .map((task) => ({
-          specialistName: specialist.name.toLowerCase(),
-          task: task.task,
-          link: task.link,
-        }));
-
-      return tasks.length > 0 ? tasks : [];
-    });
+//   const handleInputKeyDown = (e) => {
+//     switch (e.key) {
+//       case "ArrowUp":
+//         setSelectedIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+//         break;
+//       case "ArrowDown":
+//         setSelectedIndex((prevIndex) =>
+//           Math.min(prevIndex + 1, slicedResults.length - 1)
+//         );
+//         break;
+//       case "Enter":
+//         if (selectedIndex >= 0 && selectedIndex < slicedResults.length) {
+//           const selectedResult = slicedResults[selectedIndex];
+//           handleResultClick(selectedResult.link);
+//         }
+//         break;
+//       case "Tab": // Implementing autocomplete on Tab key
+//         if (slicedResults.length > 0) {
+//           const selectedResult = slicedResults[0];
+//           setValue(selectedResult.task); // Autocomplete with the first suggestion
+//           setSelectedIndex(0);
+//         }
+//         break;
+//       default:
+//         break;
+//     }
+//   };
 
   
-    const specialistResults = specialists
-      .filter((specialist) =>
-        specialist.name.toLowerCase().includes(searchTerm)
-      )
-      .map((specialist: Specialist) => ({
-        specialistName: specialist.name.toLowerCase(),
-        task: "", // Assuming a task field is required, you might want to adjust this
-        link: specialist.link || "", // Assuming a link field is required, you might want to adjust this
-      }));
+//   const searchResults = () => {
+//     const searchTerm = value.toLowerCase().trim();
 
-    return [...taskResults, ...specialistResults];
-  };
+//     // Search for matches in individual tasks and specialist names
+//     const taskResults = specialists.flatMap((specialist) => {
+//       const tasks = specialist.tasks
+//         .filter((task) => task.task.toLowerCase().includes(searchTerm))
+//         .map((task) => ({
+//           specialistName: specialist.name.toLowerCase(),
+//           task: task.task,
+//           link: task.link,
+//         }));
 
-  const slicedResults = searchResults().slice(0, 5); // Beperk tot de eerste 5 resultaten
+//       return tasks.length > 0 ? tasks : [];
+//     });
 
-  const resultsRender = slicedResults.map((result, index) => (
-    <Link
-      to={`/nl/jobs#${result.specialistName.replace('/', '')}?${result.link.replace('/', '')}`}
-      key={index}
-      className={`search_dropdown_item ${
-        index === selectedIndex ? "selected" : ""
-      }`}
-      onClick={() => handleResultClick(result.link)}
-      onMouseOver={() => setSelectedIndex(index)}
-    >
-      <span>
-        {result.specialistName ? `${result.specialistName} - ` : ""}
-        {result.task}
-      </span>
-    </Link>
-  ));
+  
+//     const specialistResults = specialists
+//       .filter((specialist) =>
+//         specialist.name.toLowerCase().includes(searchTerm)
+//       )
+//       .map((specialist: Specialist) => ({
+//         specialistName: specialist.name.toLowerCase(),
+//         task: "", // Assuming a task field is required, you might want to adjust this
+//         link: specialist.link || "", // Assuming a link field is required, you might want to adjust this
+//       }));
 
-  // Helper function to navigate to the selected result
-  const navigateToResult = (link: string) => {
-    navigate(`/nl/jobs${link}`);
-  };
+//     return [...taskResults, ...specialistResults];
+//   };
 
-  return (
-    <>
-      <div className={showList ? "search-container_open" : "search-container"}>
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Wat is je klus?"
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
-          onKeyDown={handleInputKeyDown}
-        />
-        <div className={showList ? "search_dropdown open" : "search_dropdown"}>
-          {resultsRender}
-        </div>
-      </div>
-    </>
-  );
-}
-// ...
+//   const slicedResults = searchResults().slice(0, 5); // Beperk tot de eerste 5 resultaten
 
-export default Searchbar;
+//   const resultsRender = slicedResults.map((result, index) => (
+//     <Link
+//       to={`/nl/jobs#${result.specialistName.replace('/', '')}?${result.link.replace('/', '')}`}
+//       key={index}
+//       className={`search_dropdown_item ${
+//         index === selectedIndex ? "selected" : ""
+//       }`}
+//       onClick={() => handleResultClick(result.link)}
+//       onMouseOver={() => setSelectedIndex(index)}
+//     >
+//       <span>
+//         {result.specialistName ? `${result.specialistName} - ` : ""}
+//         {result.task}
+//       </span>
+//     </Link>
+//   ));
+
+//   // Helper function to navigate to the selected result
+//   const navigateToResult = (link: string) => {
+//     navigate(`/nl/jobs${link}`);
+//   };
+
+//   return (
+//     <>
+//       <div className={showList ? "search-container_open" : "search-container"}>
+//         <input
+//           type="text"
+//           value={value}
+//           onChange={(e) => setValue(e.target.value)}
+//           placeholder="Wat is je klus?"
+//           onFocus={handleInputFocus}
+//           onBlur={handleInputBlur}
+//           onKeyDown={handleInputKeyDown}
+//         />
+//         <div className={showList ? "search_dropdown open" : "search_dropdown"}>
+//           {resultsRender}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// // ...
+
+// export default Searchbar;
