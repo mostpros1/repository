719c89681b64b3884292596dcb73d8752af8b132
Timon <timon.heidenreich@gmail.com>
2024-06-18 17:42:25 +0200

    build error

--- web/src/useUserTypeContext.tsx
+++ web/src/useUserTypeContext.tsx
@@ -1,9 +1,22 @@
 import React, { createContext, useContext, useState } from 'react';
 
+
+interface UserTypeContextType {
+  isProfessional: boolean;
+  setIsProfessional: React.Dispatch<React.SetStateAction<boolean>>;
+  isHomeowner: boolean;
+  setIsHomeowner: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
 // Create a context
-const UserTypeContext = createContext();
+const UserTypeContext = createContext<UserTypeContextType>({
+  isProfessional: false,
+  setIsProfessional: () => {},
+  isHomeowner: false,
+  setIsHomeowner: () => {},
+});
 // Create a provider component
-const UserTypeProvider = ({ children }) => {
+const UserTypeProvider = ({ children }: { children: React.ReactNode }) => {
   const [isProfessional, setIsProfessional] = useState(false);
   const [isHomeowner, setIsHomeowner] = useState(false);
 
@@ -15,7 +28,7 @@ const UserTypeProvider = ({ children }) => {
 };
 
 // Create a custom hook to use the context
-const useUserType = () => {
+const useUserType = (): UserTypeContextType => {
   return useContext(UserTypeContext);
 };
 
