bb0192bbd232e5466a0ff427fc295fde2b7496d7
Timon <timon.heidenreich@gmail.com>
2024-06-28 10:45:15 +0200

    chore: Refactor SetupPage component and fetch profile photo in useEffect

--- web/src/components/Setup/Setup.tsx
+++ web/src/components/Setup/Setup.tsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './Setup.css';
 import { Auth } from 'aws-amplify';
+import { dynamo } from '../../../declarations';
 
 const SetupPage = () => { // Removed async here
 
   // Step 1: Define a type for user data attributes
   type UserDataAttributes = {
-    email_verified?: boolean; // Marking as optional if not all users will have this attribute
+    email_verified?: boolean;
+    email?: string; // Marking as optional if not all users will have this attribute
     // Add other attributes as needed
   };
 
   // Step 2: Use the defined type when initializing useState
   const [userData, setUserData] = useState<UserDataAttributes>({});
-  const [stripeAccount, setStripeAccount] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(false);
 
 
   useEffect(() => {
@@ -34,10 +36,37 @@ const SetupPage = () => { // Removed async here
 
   console.log(userData);
 
+  useEffect(() => {
+    async function fetchProfilePhoto() {
+      try {
+        const output = await dynamo.query({
+          TableName: 'Users',
+          IndexName: 'username',
+          KeyConditionExpression: 'email = :email',
+          ExpressionAttributeValues: {
+            ':email': userData.email,
+          },
+        }).promise();
+
+        if (output && output.Items && output.Items.length > 0) {
+          const user = output.Items[0];
+          if (user.profielfoto !== undefined && user.profielfoto !== null) {
+            setProfilePhoto(!!user.profilePhoto);
+          }
+        }
+
+      } catch (error) {
+        console.error("Error fetching bio:", error);
+      }
+    }
+
+    fetchProfilePhoto();
+  }, []);
+
 
   const tasks = [
     { id: 1, description: 'Maak een Stripe-account aan', completed: !!userData['custom:stripeAccountId'] },
-    { id: 2, description: 'Voeg een profielfoto toe', completed: false },
+    { id: 2, description: 'Voeg een profielfoto toe', completed: profilePhoto },
     { id: 3, description: 'Verifieer je e-mailadres', completed: userData.email_verified },
     { id: 4, description: 'Voltooi je profielinformatie', completed: false },
   ];
