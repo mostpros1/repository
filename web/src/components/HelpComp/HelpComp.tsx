fde112d015bad16edcb8e3d0013cf82587ef22d5
Matthew Willems <matthewwillems2003@outlook.com>
2024-07-04 22:33:06 +0200

    add new FAQ design

--- web/src/components/HelpComp/HelpComp.tsx
+++ web/src/components/HelpComp/HelpComp.tsx
@@ -20,38 +20,32 @@ function HelpComp() {
         <br></br>
         <h3>Wat is Mostpros?</h3>
         <p>
-        Mostpros is een onderneming die de manier waarop huiseigenaren hun huisonderhoud en verbouwingsprojecten beheren, revolutioneert. Als een innovatief platform verbindt Mostpros huiseigenaren met bekwame vakspecialisten, waardoor de drempel voor het vinden van de juiste vakmensen voor elke klus aanzienlijk wordt verlaagd.
+          Mostpros is een onderneming die de manier waarop huiseigenaren hun huisonderhoud en verbouwingsprojecten beheren, revolutioneert. Als een innovatief platform verbindt Mostpros huiseigenaren met bekwame vakspecialisten, waardoor de drempel voor het vinden van de juiste vakmensen voor elke klus aanzienlijk wordt verlaagd.
         </p>
         <br></br>
-
         <h3>Waarom Mostpros?</h3>
         <p>
-        Begin binnen enkele minuten. Bespaar kosten met onze lage systeemkosten. Er zijn geen verborgen kosten of procentuele bezuinigingen. Bespaar administratieve tijd. Maak gebruik van onze wereldwijde betaalmethoden. Geniet van onze hartelijke, klantgerichte aanpak. Probeer het gratis uit.
-
+          Begin binnen enkele minuten. Bespaar kosten met onze lage systeemkosten. Er zijn geen verborgen kosten of procentuele bezuinigingen. Bespaar administratieve tijd. Maak gebruik van onze wereldwijde betaalmethoden. Geniet van onze hartelijke, klantgerichte aanpak. Probeer het gratis uit.
         </p>
         <br></br>
         <h3>Hoe maak ik een account aan als huiseigenaar?</h3>
         <p>
           Om een ​​account aan te maken als huiseigenaar, volgt u het offerteproces op de startpagina van Mostpros. Zodra alle stappen zijn voltooid, hebt u de mogelijkheid om een ​​account aan te maken of in te loggen als u er al een hebt.
-
         </p>
         <br></br>
         <h3>Hoe maak ik een account aan als specialist?</h3>
         <p>
           Om een ​​account aan te maken als specialist, klikt u op "Inschrijven als vakspecialist" in de navigatiebalk op de homepage. Volg de stappen die vergelijkbaar zijn met het proces voor huiseigenaren. Uiteindelijk bereikt u dezelfde fase waarin u een account kunt aanmaken of kunt inloggen met het e-mailadres dat u eerder hebt opgegeven.
-
         </p>
         <br></br>
         <h3>Hoe werkt Mostpros voor huiseigenaren?</h3>
         <p>
           Mostpros is toegankelijk via het web of via een mobiele app. Om alle functies te gebruiken, moeten gebruikers inloggen of een account aanmaken. Als huiseigenaar kunt u uw beschikbaarheid voor een klus opgeven. Mostpros zal vervolgens uw behoeften matchen met een geschikte professional.
-
         </p>
         <br></br>
         <h3>Een opdracht plaatsen op Mostpros</h3>
         <p>
           Bij Mostpros streven we ernaar om het vinden van de juiste professional voor uw project zo eenvoudig mogelijk te maken. Volg deze stappen om uw opdracht op ons platform te plaatsen en aan de slag te gaan:
-
         </p>
         <br></br>
         <p>
@@ -71,13 +65,11 @@ function HelpComp() {
           7. Laat een beoordeling achter: Uw feedback is waardevol voor ons. Nadat de klus is afgerond, kunt u een beoordeling achterlaten voor de specialist, zodat anderen weloverwogen beslissingen kunnen nemen.
           <br></br>
           Met Mostpros is het beheren van uw woningprojecten eenvoudiger dan ooit. Probeer het vandaag nog en ervaar het gemak van ons platform voor zowel huiseigenaren als specialisten!
-
         </p>
         <br></br>
         <h3>Hoe werkt Mostpros voor specialisten?</h3>
         <p>
           Mostpros is toegankelijk via het web of een mobiele app. Gebruikers moeten inloggen of een account aanmaken om alle functies te gebruiken. Bezoek de Mostpros-website en selecteer "Inschrijven als vakspecialist" rechtsboven. Volg de stappen om een ​​account aan te maken en Mostpros te gaan gebruiken.
-
         </p>
         <br></br>
         <br></br>
@@ -104,7 +96,6 @@ function HelpComp() {
         <h3>Hoe werkt Mostpros voor specialisten?</h3>
         <p>
           Mostpros is toegankelijk via het web of een mobiele app. Gebruikers moeten inloggen of een account aanmaken om alle functies te gebruiken. Bezoek de Mostpros-website en selecteer "Inschrijven als vakspecialist" rechtsboven. Volg de stappen om een ​​account aan te maken en Mostpros te gaan gebruiken.
-
         </p>
         <br></br>
         <h3>Hoe wijzig ik mijn wachtwoord?</h3>
@@ -143,6 +134,25 @@ function HelpComp() {
         <br></br>
         <h3>Ik heb mijn klus geplaatst. Wat zijn de volgende stappen?</h3>
         <p>Nadat u uw klus heeft geplaatst, ontvangt u meldingen wanneer specialisten interesse tonen. U kunt met hen communiceren via de chatfunctie, details bespreken en een overeenkomst sluiten.
+        </p>
+        <br></br>
+        <br></br>
+        <h2>Probleemoplossing</h2>
+        <br></br>
+        <h3>De website werkt niet. Wat moet ik doen?</h3>
+        <p>Als u technische problemen ondervindt, controleer eerst uw internetverbinding en probeer de pagina te vernieuwen. Als het probleem aanhoudt, wis dan de cache van uw browser of probeer de site te openen via een andere browser of apparaat. Neem bij aanhoudende problemen contact op met de klantenservice van Mostpros voor assistentie.
+        </p>
+        <br></br>
+        <h3>Ik heb geen bevestigingsmail ontvangen. Wat moet ik doen?</h3>
+        <p>Controleer uw spam- of junkmap voor de bevestigingsmail. Als deze daar niet staat, zorg er dan voor dat u het juiste e-mailadres heeft ingevoerd. U kunt een nieuwe bevestigingsmail aanvragen door naar de inlogpagina te gaan en "Bevestigingsmail opnieuw verzenden" te selecteren. Als u nog steeds problemen ondervindt, neem dan contact op met de klantenservice voor verdere hulp.
+        </p>
+        <br></br>
+        <h3>Ik kan niet inloggen op mijn account. Wat moet ik doen?</h3>
+        <p>Als u problemen heeft met inloggen, controleer dan eerst of uw inloggegevens correct zijn. Gebruik de functie "Wachtwoord vergeten" om uw wachtwoord indien nodig opnieuw in te stellen. Als het probleem aanhoudt, zorg er dan voor dat uw account geverifieerd en niet geschorst is. Neem voor verdere assistentie contact op met de klantenservice van Mostpros.
+        </p>
+        <br></br>
+        <h3>Wat als ik hulp nodig heb bij iets dat niet in de FAQ staat?</h3>
+        <p>Als u hulp nodig heeft bij een probleem dat niet in de FAQ wordt behandeld, neem dan contact op met de klantenservice van Mostpros. Ons team staat klaar om u te helpen met eventuele vragen of zorgen die u heeft. Stuur uw mail naar teammostpros@gmail.com
         </p>
         <br></br>
       </div>
